8cc2d18713c96d4fa421a2f3cec542325f3f85d5
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 09:35:21 +0100

    Update ExpenseRepository.php

--- app/Repositories/ExpenseRepository.php
+++ app/Repositories/ExpenseRepository.php
@@ -38,35 +38,11 @@ class ExpenseRepository extends BaseRepository
         return $this->findOneOrFail($id);
     }
 
-    /**
-     * Saves the expense and its contacts
-     *
-     * @param array $data The data
-     * @param \App\Models\expense $expense The expense
-     *
-     * @return     expense|\App\Models\expense|null  expense Object
-     */
     public function save(array $data, Expense $expense): ?Expense
     {
         $expense->fill($data);
-
         $expense->save();
 
-// if ($expense->id_number == "" || !$expense->id_number) {
-//     $expense->id_number = $this->getNextExpenseNumber($expense);
-// } //todo write tests for this and make sure that custom expense numbers also works as expected from here
-
         return $expense;
-    }
-
-    /**
-     * Store expenses in bulk.
-     *
-     * @param array $expense
-     * @return expense|null
-     */
-    public function create($expense): ?Expense
-    {
-        return $this->save($expense, ExpenseFactory::create(auth()->user()->company()->id, auth()->user()->id));
     }
 }
