df85cea0c74198ff4cc4197626f2a050a1c691a6
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:35:05 +0100

    Update ProductFactory.php

--- app/Factory/ProductFactory.php
+++ app/Factory/ProductFactory.php
@@ -11,19 +11,8 @@ class ProductFactory
         $product = new Product;
         $product->user_id = $user_id;
         $product->account_id = $account_id;
-        $product->company_id = 0;
-        $product->sku = '';
-        $product->name = '';
-        $product->slug = '';
-        $product->description = '';
-        $product->cost = 0;
-        $product->price = 0;
         $product->quantity = 1;
-        $product->cover = '';
-        $product->custom_value1 = '';
-        $product->custom_value2 = '';
-        $product->custom_value3 = '';
-        $product->custom_value4 = '';
+     
         return $product;
     }
 }
