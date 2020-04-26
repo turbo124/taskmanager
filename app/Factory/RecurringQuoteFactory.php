d2610a610847658d178052961f4a58eb1b8a7ea3
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 16:06:24 +0100

    Update RecurringQuoteFactory.php

--- app/Factory/RecurringQuoteFactory.php
+++ app/Factory/RecurringQuoteFactory.php
@@ -3,18 +3,21 @@
 namespace App\Factory;
 
 use App\RecurringQuote;
+use App\Account;
+use App\User;
+use App\Customer;
 
 class RecurringQuoteFactory
 {
-    public static function create(int $customer_id, int $account_id, int $total): RecurringQuote
+    public static function create(Customer $customer, Account $account, int $total, User $user): RecurringQuote
     {
         $quote = new RecurringQuote();
-        $quote->account_id = $account_id;
+        $quote->account_id = $account->id;
         $quote->status_id = RecurringQuote::STATUS_DRAFT;
         $quote->total = $total;
         $quote->balance = $total;
-        $quote->user_id = auth()->user()->id;
-        $quote->customer_id = $customer_id;
+        $quote->user_id = $user->id;
+        $quote->customer_id = $customer->id;
         $quote->frequency_id = RecurringQuote::FREQUENCY_MONTHLY;
      
         return $quote;
