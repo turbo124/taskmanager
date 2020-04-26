c5096e009a1c5e24d8b3a023f26fa299a668a47e
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 16:02:00 +0100

    Update OrderFactory.php

--- app/Factory/OrderFactory.php
+++ app/Factory/OrderFactory.php
@@ -2,6 +2,8 @@
 
 namespace App\Factory;
 
+use App\Account;
+use App\User;
 use App\Customer;
 use App\Order;
 
@@ -20,14 +22,15 @@ class OrderFactory
      * @param Customer|null $customer
      * @return Invoice
      */
-    public static function create(int $account_id,
-        int $user_id,
+    public static function create(Account $account,
+        User $user,
         Customer $customer): Order
     {
         $order = new Order;
-        $order->account_id = $account_id;
+        $order->account_id = $account->id;
         $order->status_id = Order::STATUS_DRAFT;
         $order->customer_id = $customer->id;
+        $order->user_id = $user->id;
 
         return $order;
     }
