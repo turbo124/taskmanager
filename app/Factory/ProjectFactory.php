57759777e220d955ab7e584f3437e267f56d3e37
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:35:38 +0100

    Update ProjectFactory.php

--- app/Factory/ProjectFactory.php
+++ app/Factory/ProjectFactory.php
@@ -9,14 +9,11 @@ class ProjectFactory
     public static function create(int $user_id, int $customer_id, int $account_id): Project
     {
         $project = new Project;
-        $project->title = '';
-        $project->description = '';
-        $project->notes = '';
-        $project->due_date = null;
-        $project->budgeted_hours = null;
+  
         $project->customer_id = $customer_id;
         $project->account_id = $account_id;
         $project->user_id = $user_id;
+        
         return $project;
     }
 }
