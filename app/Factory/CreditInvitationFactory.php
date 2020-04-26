1aa9c8db7c79bc6d77b7291c996070357cf22b7a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:29:01 +0100

    Update CreditInvitationFactory.php

--- app/Factory/CreditInvitationFactory.php
+++ app/Factory/CreditInvitationFactory.php
@@ -12,17 +12,7 @@ class CreditInvitationFactory
         $ci = new CreditInvitation;
         $ci->account_id = $account_id;
         $ci->user_id = $user_id;
-        $ci->client_contact_id = null;
-        $ci->credit_id = null;
         $ci->key = Str::random(40);
-        $ci->transaction_reference = null;
-        $ci->message_id = null;
-        $ci->email_error = '';
-        $ci->signature_base64 = '';
-        $ci->signature_date = null;
-        $ci->sent_date = null;
-        $ci->viewed_date = null;
-        $ci->opened_date = null;
 
         return $ci;
     }
