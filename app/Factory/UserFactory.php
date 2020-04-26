addcfd65e0b67134e23b2d8d01e85110700b130b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:41:44 +0100

    Update UserFactory.php

--- app/Factory/UserFactory.php
+++ app/Factory/UserFactory.php
@@ -10,18 +10,10 @@ class UserFactory
     public static function create(int $domain_id): User
     {
         $user = new User;
-        $user->first_name = '';
         $user->last_login = now();
         $user->ip = request()->ip();
         $user->confirmation_code = Str::random(config('taskmanager.key_length'));
         $user->domain_id = $domain_id;
-        $user->last_name = '';
-        $user->phone_number = '';
-        $user->username = '';
-        $user->email = '';
-        $user->gender = '';
-        $user->dob = null;
-        $user->job_description = '';
         $user->is_active = 1;
 
         return $user;
