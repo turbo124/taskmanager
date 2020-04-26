184d52fa4fe94fb9732ad376e263c73922e27b74
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:26:41 +0100

    Update InvoiceFactory.php

--- app/Factory/InvoiceFactory.php
+++ app/Factory/InvoiceFactory.php
@@ -23,26 +23,9 @@ class InvoiceFactory
     {
         $invoice = new Invoice();
         $invoice->account_id = $account_id;
-        $invoice->balance = 0;
         $invoice->status_id = Invoice::STATUS_DRAFT;
-        $invoice->discount_total = 0;
-        $invoice->tax_total = 0;
-        $invoice->footer = '';
-        $invoice->terms = '';
-        $invoice->public_notes = '';
-        $invoice->private_notes = '';
-        $invoice->tax_rate_name = '';
-        $invoice->tax_rate = 0;
-        $invoice->date = null;
-        $invoice->partial_due_date = null;
-        $invoice->total = 0;
         $invoice->user_id = $user_id;
-        $invoice->partial = 0;
         $invoice->customer_id = $customer->id;
-        $invoice->custom_value1 = '';
-        $invoice->custom_value2 = '';
-        $invoice->custom_value3 = '';
-        $invoice->custom_value4 = '';
 
         return $invoice;
     }
