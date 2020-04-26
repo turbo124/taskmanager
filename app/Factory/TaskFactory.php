75d816f96cb7c68bb20762cb4ca5d01496fa25e9
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 16:00:29 +0100

    Update TaskFactory.php

--- app/Factory/TaskFactory.php
+++ app/Factory/TaskFactory.php
@@ -3,16 +3,19 @@
 namespace App\Factory;
 
 use App\Task;
+use App\Account;
+use App\User;
+use App\Customer;
 
 class TaskFactory
 {
-    public static function create(int $user_id, int $account_id): Task
+    public static function create(User $user, Account $account): Task
     {
         $task = new Task;
         $task->source_type = 1;
-        $task->user_id = $user_id;
-        $task->created_by = $user_id;
-        $task->account_id = $account_id;
+        $task->user_id = $user->id;
+        $task->created_by = $user->id;
+        $task->account_id = $account->id;
 
         return $task;
     }
