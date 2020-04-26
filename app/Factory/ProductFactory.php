785dd42b1696172e32a92a60171f8d5e69444b34
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 16:13:01 +0100

    Update ProductFactory.php

--- app/Factory/ProductFactory.php
+++ app/Factory/ProductFactory.php
@@ -3,14 +3,17 @@
 namespace App\Factory;
 
 use App\Product;
+use App\Account;
+use App\User;
+use App\Customer;
 
 class ProductFactory
 {
-    public static function create(int $user_id, int $account_id): Product
+    public static function create(User $user, Account $account): Product
     {
         $product = new Product;
-        $product->user_id = $user_id;
-        $product->account_id = $account_id;
+        $product->user_id = $user->id;
+        $product->account_id = $account->id;
         $product->quantity = 1;
      
         return $product;
