271292e5f6900ab5b2823ed7dabb20bea8ec3acb
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:33:43 +0100

    Update OrderInvitationFactory.php

--- app/Factory/OrderInvitationFactory.php
+++ app/Factory/OrderInvitationFactory.php
@@ -13,17 +13,8 @@ class OrderInvitationFactory
         $qi = new OrderInvitation;
         $qi->account_id = $account_id;
         $qi->user_id = $user_id;
-        $qi->client_contact_id = null;
-        $qi->order_id = null;
         $qi->key = Str::random(20);
-        $qi->transaction_reference = null;
-        $qi->message_id = null;
-        $qi->email_error = '';
-        $qi->signature_base64 = '';
-        $qi->signature_date = null;
-        $qi->sent_date = null;
-        $qi->viewed_date = null;
-        $qi->opened_date = null;
+        
         return $qi;
     }
 
