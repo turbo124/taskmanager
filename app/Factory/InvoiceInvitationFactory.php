2ac3fbe3a45d6aa9b9f947283b49edfd8891683b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:32:09 +0100

    Update InvoiceInvitationFactory.php

--- app/Factory/InvoiceInvitationFactory.php
+++ app/Factory/InvoiceInvitationFactory.php
@@ -12,17 +12,8 @@ class InvoiceInvitationFactory
         $ii = new InvoiceInvitation;
         $ii->account_id = $account_id;
         $ii->user_id = $user_id;
-        $ii->client_contact_id = null;
-        $ii->invoice_id = null;
         $ii->key = Str::random(20);
-        $ii->transaction_reference = null;
-        $ii->message_id = null;
-        $ii->email_error = '';
-        $ii->signature_base64 = '';
-        $ii->signature_date = null;
-        $ii->sent_date = null;
-        $ii->viewed_date = null;
-        $ii->opened_date = null;
+   
         return $ii;
     }
 
