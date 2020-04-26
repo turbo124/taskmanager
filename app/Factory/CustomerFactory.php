3de05bd578a0ee33cb1acfb18a5b3ce1465ee227
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 16:42:24 +0100

    Update CustomerFactory.php

--- app/Factory/CustomerFactory.php
+++ app/Factory/CustomerFactory.php
@@ -9,12 +9,11 @@ use App\Settings;
 
 class CustomerFactory
 {
-    public static function create(Account $account, User $user, Company $company = null): Customer
+    public static function create(Account $account, User $user): Customer
     {
         $client = new Customer;
         $client->currency_id = 2;
         $client->account_id = $account->id;
-        $client->company_id = $company->id;
         $client->user_id = $user->id;
         $client->settings = (new Settings)->getAccountDefaults();
 
