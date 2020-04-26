42b9b6e0805a5893782446182b4dca49bb27100f
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:59:13 +0100

    Update CreditFactory.php

--- app/Factory/CreditFactory.php
+++ app/Factory/CreditFactory.php
@@ -3,21 +3,23 @@
 namespace App\Factory;
 
 use App\Credit;
+use App\Account;
+use App\User;
 use App\Customer;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 
 class CreditFactory
 {
-    public static function create(int $account_id,
-        int $user_id,
+    public static function create(Account $account,
+        User $user,
         Customer $customer): Credit
     {
         $credit = new Credit;
         $credit->status_id = Credit::STATUS_DRAFT;
         $credit->customer_id = $customer->id;
-        $credit->account_id = $account_id;
-        $credit->user_id = $user_id;
+        $credit->account_id = $account->id;
+        $credit->user_id = $user->id;
         $credit->tax_rate = 0;
         return $credit;
     }
