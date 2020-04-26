bef412d5f2630dcacf02c631ff80f744a006d13f
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-26 15:25:00 +0100

    Update CreditFactory.php

--- app/Factory/CreditFactory.php
+++ app/Factory/CreditFactory.php
@@ -15,16 +15,9 @@ class CreditFactory
     {
         $credit = new Credit;
         $credit->status_id = Credit::STATUS_DRAFT;
-        $credit->balance = 0;
         $credit->customer_id = $customer->id;
         $credit->account_id = $account_id;
-        $credit->total = 0;
         $credit->user_id = $user_id;
-        $credit->footer = '';
-        $credit->terms = '';
-        $credit->public_notes = '';
-        $credit->private_notes = '';
-        $credit->tax_rate_name = '';
         $credit->tax_rate = 0;
         return $credit;
     }
