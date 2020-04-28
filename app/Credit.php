bd1c3878529da54236db9f77e5d5ccc134aadb74
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-28 20:00:14 +0100

    Update Credit.php

--- app/Credit.php
+++ app/Credit.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
 use Laracasts\Presenter\PresentableTrait;
+use App\NumberGenerator;
 
 class Credit extends Model
 {
