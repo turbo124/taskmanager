9939d43b5d5ccbfae64b194e8e4bb58e7bbb63d5
michael-hampton <michaelhamptondesign@yahoo.com>
2020-04-29 19:45:51 +0100

    Update CloneInvoiceFactory.php

--- app/Factory/CloneInvoiceFactory.php
+++ app/Factory/CloneInvoiceFactory.php
@@ -3,22 +3,20 @@
 namespace App\Factory;
 
 use App\Invoice;
+use App\User;
+use App\Account;
 
 class CloneInvoiceFactory
 {
-    public static function create($invoice, $user_id, $account_id)
+    public static function create(Invoice $invoice, User $user, Account $account)
     {
-        $clone_invoice = new Invoice();
-        $clone_invoice->account_id = $account_id;
+        $clone_invoice = $invoice->replicate();
+        $clone_invoice->account_id = $account->id;
         $clone_invoice->status_id = Invoice::STATUS_DRAFT;
         $clone_invoice->number = null;
-        $clone_invoice->date = $invoice->date;
-        $clone_invoice->due_date = $invoice->due_date;
         $clone_invoice->partial_due_date = null;
-        $clone_invoice->user_id = $user_id;
-        $clone_invoice->total = $invoice->total;
+        $clone_invoice->user_id = $user->id;
         $clone_invoice->balance = $invoice->total;
-        $clone_invoice->line_items = $invoice->line_items;
 
         return $clone_invoice;
     }
