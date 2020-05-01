c44ba65d4d34cb3b9afc5c70f0c3de4e6e9d22d1
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-01 20:49:46 +0100

    Update Info.php

--- app/Designs/Info.php
+++ app/Designs/Info.php
@@ -51,7 +51,8 @@ $table_here
             <span style="margin-right: 20px">$subtotal_label</span> $subtotal<br>
             <span style="margin-right: 20px">$discount_label</span> $discount<br>
             <span style="margin-right: 20px">$tax_label</span> $tax<br>
-            <span style="margin-right: 20px">$balance_due_label</span> $balance_due<br>
+            <span class="font-weight-bold" style="margin-right: 20px">$balance_due_label</span> 
+            <span class="text-info"> $balance_due</span><br>
         </div>
     </div>
 </div>
