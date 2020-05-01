93c2d39e8080b057c05a064ca795b27f98499369
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-01 20:14:17 +0100

    Update Secondary.php

--- app/Designs/Secondary.php
+++ app/Designs/Secondary.php
@@ -66,7 +66,7 @@ $table_here
 <div class="mt-4 pb-4 px-4" style="width: 100%">
 <div class="inline-block" style="width: 70%">
     <div class="">
-        <p class="font-weight-bolder">$terms_label</p>
+        <p class="font-weight-bold">$terms_label</p>
         <p>$terms</p>
     </div>
 </div>
