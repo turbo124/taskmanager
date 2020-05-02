5e728c4cfcfbb5020ac58d4714f95875df57f3b1
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-02 10:05:31 +0100

    Update CustomerSettings.php

--- app/Settings/CustomerSettings.php
+++ app/Settings/CustomerSettings.php
@@ -1,1 +1,17 @@
+<?php
+namespace App\Settings;
 
+class CustomerSettings extends BaseSettings
+{
+        private $settings = [
+            'currency_id'                        => ['required' => true, 'default_value' => 2, 'type' => 'string'],
+            'payment_terms'                      => ['required' => false, 'default_value' => -1, 'type' => 'integer'],
+            'payment_type_id'                    => ['required' => false, 'default_value' => 0, 'type' => 'string']
+        ];
+
+    public function save(Customer $customer, $settings)
+    {
+
+
+    }
+}
