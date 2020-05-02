61e8cd0f7766945250aa41e7bff2a886d589f699
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-02 10:07:22 +0100

    Update GatewaySettings.php

--- app/Settings/GatewaySettings.php
+++ app/Settings/GatewaySettings.php
@@ -1,1 +1,27 @@
+<?php
+namespace App\Settings;
 
+class GatewaySettings extends BaseSettings
+{
+        private $settings = [
+            'gateway_type_id'    => ['required' => false, 'default_value' => 1, 'type' => 'int'],
+            'min_limit'          => ['required' => false, 'default_value' => -1, 'type' => 'float'],
+            'max_limit'          => ['required' => false, 'default_value' => -1, 'type' => 'float'],
+            'fee_amount'         => ['required' => false, 'default_value' => 0, 'type' => 'float'],
+            'fee_percent'        => ['required' => false, 'default_value' => 0, 'type' => 'float'],
+            'fee_tax_name1'      => ['required' => false, 'default_value' => '', 'type' => 'string'],
+            'fee_tax_name2'      => ['required' => false, 'default_value' => '', 'type' => 'string'],
+            'fee_tax_name3'      => ['required' => false, 'default_value' => '', 'type' => 'string'],
+            'fee_tax_rate1'      => ['required' => false, 'default_value' => 0, 'type' => 'float'],
+            'fee_tax_rate2'      => ['required' => false, 'default_value' => 0, 'type' => 'float'],
+            'fee_tax_rate3'      => ['required' => false, 'default_value' => 0, 'type' => 'float'],
+            'fee_cap'            => ['required' => false, 'default_value' => 0, 'type' => 'float'],
+            'adjust_fee_percent' => ['required' => false, 'default_value' => false, 'type' => 'bool'],
+        ];
+
+    public function save(CompanyGateway $company_gateway, $settings)
+    {
+
+    }
+
+}
