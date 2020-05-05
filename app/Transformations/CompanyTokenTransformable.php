7e2bf1e709afdf6bc1df87f5ba6f2618b62a0b47
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-05 22:00:31 +0100

    Update CompanyTokenTransformable.php

--- app/Transformations/CompanyTokenTransformable.php
+++ app/Transformations/CompanyTokenTransformable.php
@@ -15,14 +15,13 @@ trait CompanyTokenTransformable
      */
     public function transform(CompanyToken $company_token)
     {
-        $prod = new CompanyToken;
-        $prod->id = (int)$company_token->id;
-        $prod->name = $company_token->name ?: '';
-        $prod->updated_at = (int)$company_token->updated_at;
-        $prod->archived_at = (int)$company_token->deleted_at;
-        $prod->created_at = (int)$company_token->created_at;
-        $prod->is_deleted = (bool)$company_token->is_deleted;
-
-        return $prod;
+        return [
+        'id' => (int)$company_token->id,
+        'name' => $company_token->name ?: '',
+        'updated_at' => (int)$company_token->updated_at,
+        'archived_at' => (int)$company_token->deleted_at,
+        'created_at' => (int)$company_token->created_at,
+        'is_deleted' => (bool)$company_token->is_deleted,
+        ];
     }
 }
