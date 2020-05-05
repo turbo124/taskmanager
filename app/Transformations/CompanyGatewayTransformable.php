69d049ad979bbeee1da7db6db69a682c0b888e87
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-05 21:56:03 +0100

    Update CompanyGatewayTransformable.php

--- app/Transformations/CompanyGatewayTransformable.php
+++ app/Transformations/CompanyGatewayTransformable.php
@@ -8,18 +8,18 @@ trait CompanyGatewayTransformable
 {
     protected function transformCompanyGateway(CompanyGateway $company_gateway)
     {
-        $prop = new CompanyGateway;
-        $prop->id = (int)$company_gateway->id;
-        $prop->gateway_key = (string)$company_gateway->gateway_key ?: '';
-        $prop->accepted_credit_cards = $company_gateway->accepted_credit_cards;
-        $prop->require_cvv = (bool)$company_gateway->require_cvv;
-        $prop->show_billing_address = (bool)$company_gateway->show_billing_address;
-        $prop->show_shipping_address = (bool)$company_gateway->show_shipping_address;
-        $prop->update_details = (bool)$company_gateway->update_details;
-        $prop->config = $company_gateway->config;
-        $prop->fees_and_limits = $company_gateway->fees_and_limits ?: '';
-        $prop->updated_at = $company_gateway->updated_at;
-        $prop->deleted_at = $company_gateway->deleted_at;
-        return $prop;
+        return [
+        'id' => (int)$company_gateway->id,
+        'gateway_key' => (string)$company_gateway->gateway_key ?: '',
+        'accepted_credit_cards' => $company_gateway->accepted_credit_cards,
+        'require_cvv' => (bool)$company_gateway->require_cvv,
+        'show_billing_address' => (bool)$company_gateway->show_billing_address,
+        'show_shipping_address' => (bool)$company_gateway->show_shipping_address,
+        'update_details' => (bool)$company_gateway->update_details,
+        'config' => $company_gateway->config,
+        'fees_and_limits' => $company_gateway->fees_and_limits ?: '',
+        'updated_at' => $company_gateway->updated_at,
+        'deleted_at' => $company_gateway->deleted_at,
+        ];
     }
 }
