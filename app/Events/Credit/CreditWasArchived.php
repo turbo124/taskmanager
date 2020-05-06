f228026311c059e3146dd45683953505bda2e41b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-06 20:06:17 +0100

    Update CreditWasArchived.php

--- app/Events/Credit/CreditWasArchived.php
+++ app/Events/Credit/CreditWasArchived.php
@@ -3,19 +3,20 @@
 namespace App\Events\Credit;
 
 use Illuminate\Queue\SerializesModels;
+use App\Credit;
 
 class CreditWasArchived
 {
     use SerializesModels;
 
-    public $credit;
+    public Credit $credit;
 
     /**
      * Create a new event instance.
      *
      * @param $credit
      */
-    public function __construct($credit)
+    public function __construct(Credit $credit)
     {
         $this->credit = $credit;
     }
