e26a51b8eb8a5b8c6155ae455c2c55c84a782906
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-06 20:12:17 +0100

    Update QuoteWasDeleted.php

--- app/Events/Quote/QuoteWasDeleted.php
+++ app/Events/Quote/QuoteWasDeleted.php
@@ -4,6 +4,7 @@ namespace App\Events\Quote;
 
 use App\Traits\SendSubscription;
 use Illuminate\Queue\SerializesModels;
+use App\Quote;
 
 /**
  * Class QuoteWasDeleted.
@@ -13,14 +14,14 @@ class QuoteWasDeleted
     use SerializesModels;
     use SendSubscription;
 
-    public $quote;
+    public Quote $quote;
 
     /**
      * Create a new event instance.
      *
      * @param $quote
      */
-    public function __construct($quote)
+    public function __construct(Quote $quote)
     {
         $this->quote = $quote;
         $this->send($quote, get_class($this));
