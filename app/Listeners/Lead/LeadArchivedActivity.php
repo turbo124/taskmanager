dcb6a35f62f4bb7eec20c69922039c7b6f556508
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-06 20:15:52 +0100

    Create LeadArchivedActivity.php

---
+++ app/Listeners/Lead/LeadArchivedActivity.php
@@ -0,0 +1,46 @@
+
+<?php
+
+namespace App\Listeners\Lead;
+
+use App\Factory\NotificationFactory;
+use App\Repositories\NotificationRepository;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class LeadArchivedActivity implements ShouldQueue
+{
+    protected $notification_repo;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(NotificationRepository $notification_repo)
+    {
+        $this->notification_repo = $notification_repo;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param object $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $fields = [];
+        $fields['data']['id'] = $event->lead->id;
+        $fields['data']['message'] = 'A lead was archived';
+        $fields['notifiable_id'] = $event->lead->user_id;
+        $fields['account_id'] = $event->lead->account_id;
+        $fields['notifiable_type'] = get_class($event->lead);
+        $fields['type'] = get_class($this);
+        $fields['data'] = json_encode($fields['data']);
+
+        $notification = NotificationFactory::create($event->lead->account_id, $event->lead->user_id);
+        $this->notification_repo->save($notification, $fields);
+    }
+}
