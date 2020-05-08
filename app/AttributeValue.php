91d3af394d6cec9b894c2d19556843a726fb3b6a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-08 15:03:20 +0100

    Create AttributeValue.php

---
+++ app/AttributeValue.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use App\Attribute;
+use App\ProductAttribute;
+use Illuminate\Database\Eloquent\Model;
+
+class AttributeValue extends Model
+{
+    protected $fillable = [
+        'value'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function attribute()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function productAttributes()
+    {
+        return $this->belongsToMany(ProductAttribute::class);
+    }
+}
