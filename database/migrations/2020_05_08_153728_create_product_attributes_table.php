c6636ff82237b9e85641b567ddab007819a1548f
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-08 16:42:23 +0100

    fixes for tests

---
+++ database/migrations/2020_05_08_153728_create_product_attributes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateProductAttributesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('product_attributes', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('quantity');
+			$table->decimal('price')->nullable();
+			$table->integer('product_id')->unsigned()->index('product_attributes_product_id_foreign');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('product_attributes');
+	}
+
+}
