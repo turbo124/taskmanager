fc1e5bd6da72420b1f30aeec9ea8bc2dc0b3c4fe
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-10 14:10:43 +0100

    Create RefundNotification.php

---
+++ app/Notifications/Admin/RefundNotification.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Notifications\Admin;
+
+use App\Utils\Number;
+use App\Payment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\SlackMessage;
+
+class RefundNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+
+    private Payment $payment;
+
+    private string $message_type;
+
+    public function __construct(Payment $payment, $message_type = '')
+    {
+        $this->payment = $payment;
+        $this->message_type = $message_type;
+    }
+
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return !empty($this->message_type) ? [$this->message_type] : [$notifiable->account_user()->default_notification_type];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)->subject(trans('texts.notification_refund_subject',
+            ['customer' => $this->payment->customer->present()->name(),]))->markdown('email.admin.new', ['data' => [
+            'title'       => trans('texts.notification_refund_subject',
+                ['customer' => $this->payment->customer->present()->name()]),
+            'message'     => trans('texts.notification_payment_paid', $this->getDataArray()),
+            'signature'   => isset($this->payment->account->settings->email_signature) ? $this->payment->account->settings->email_signature : '',
+            'url'         => config('taskmanager.site_url') . 'portal/payments/' . $this->payment->id,
+            'button_text' => trans('texts.view_payment'),
+            'logo'        => $this->payment->account->present()->logo(),
+        ]]);
+    }
+
+    private function getDataArray()
+    {
+        return [
+            'total'    => $this->payment->getFormattedAmount(),
+            'customer' => $this->payment->customer->present()->name(),
+            'invoice'  => $this->payment->getFormattedInvoices(),
+            'payment'  => $this->payment->number,
+       ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [//
+        ];
+    }
+
+    public function toSlack($notifiable)
+    {
+
+        return (new SlackMessage)->success()
+                                 ->from("System")->image($this->account->present()->logo())->content(trans('texts.notification_refund', $this->getDataArray()));
+    }
+
+}
