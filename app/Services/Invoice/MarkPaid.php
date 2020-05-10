db96a5e55cf063d59e2a0df18d372c76331f6f30
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-10 14:32:12 +0100

    Update MarkPaid.php

--- app/Services/Invoice/MarkPaid.php
+++ app/Services/Invoice/MarkPaid.php
@@ -24,8 +24,21 @@ class MarkPaid
             return false;
         }
 
-        /* Create Payment */
-        $payment = $this->payment_repo->save(
+        // create payment
+        $payment = $this->createPayment();
+
+        // update invoice 
+        $this->updateInvoice($payment);
+
+        // update customer
+        $this->updateCustomer();
+
+        return $this->invoice;
+    }
+
+    private function createPayment(): Payment
+    {
+         $payment = $this->payment_repo->save(
             [
                 'transaction_reference' => trans('texts.manual')
             ],
@@ -35,19 +48,25 @@ class MarkPaid
         // attach invoices to payment
         $payment = $payment->attachInvoice($this->invoice);
 
-        // update balance and status
-        $new_balance = $this->invoice->balance += floatval($payment->amount * -1);
-        $this->invoice->setBalance($new_balance);
-        $this->invoice->setStatus(Invoice::STATUS_PAID);
-        $this->invoice->save();
+        return $payment;
+    }
 
-        // update customer
+    private function updateCustomer(Payment $payment)
+    {
         $customer = $this->invoice->customer;
         $customer->setBalance($payment->amount * -1);
         $customer->setPaidToDate($payment->amount);
         $customer->save();
+        return true;
+    }
 
-        return $this->invoice;
+    private function updateInvoice(Payment $payment)
+    {
+        $new_balance = $this->invoice->balance += floatval($payment->amount * -1);
+        $this->invoice->setBalance($new_balance);
+        $this->invoice->setStatus(Invoice::STATUS_PAID);
+        $this->invoice->save();
+        return true;
     }
 
 }
