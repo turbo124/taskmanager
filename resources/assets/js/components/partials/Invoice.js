023a3d48f8877a293f524424bd9e91823c0440dd
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-23 00:49:08 +0100

    Update Invoice.js

--- resources/assets/js/components/partials/Invoice.js
+++ resources/assets/js/components/partials/Invoice.js
@@ -155,8 +155,10 @@ export default class Invoice extends Component {
             <Col sm="6">
               <Card body>
                 <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
+                  <CardText>
+                      <FileUploads entity_type="Invoice" entity={this.props.entity}
+                    user_id={this.props.entity.user_id}/>
+                </CardText>
               </Card>
             </Col>
           </Row>
