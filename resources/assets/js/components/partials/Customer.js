c0156e75cd3b104131faf2e5448d933eeb8378c2
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-23 00:39:28 +0100

    Update Customer.js

--- resources/assets/js/components/partials/Customer.js
+++ resources/assets/js/components/partials/Customer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {
+    Row,
     Card,
     CardText
 } from 'reactstrap'
@@ -25,6 +26,29 @@ export default class Customer extends Component {
                                 amount={this.props.entity.balance} />}
                         </div>
                     </div>
+
+                    <Row>
+                          <ListGroup>
+      <ListGroupItem>
+        <ListGroupItemHeading> 
+            {this.props.entity.first_name} {this.props.entity.last_name} <br />
+            {this.props.entity.email}
+        </ListGroupItemHeading>
+        <ListGroupItemText>
+         Email
+        </ListGroupItemText>
+      </ListGroupItem>
+       <ListGroupItem>
+        <ListGroupItemHeading> 
+            {this.props.entity.first_name} {this.props.entity.last_name} <br />
+            {this.props.entity.phone}
+        </ListGroupItemHeading>
+        <ListGroupItemText>
+         Phone
+        </ListGroupItemText>
+      </ListGroupItem>
+    </ListGroup>
+                    </Row>
                 </CardText>
             </Card>
         )
