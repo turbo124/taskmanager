9db4719e85e5aab12a935d58c0ced523554ea141
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-23 09:17:08 +0100

    Create PaymentTermsController.php

---
+++ app/Http/Controllers/PaymentTermsController.php
@@ -0,0 +1,122 @@
+
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Factory\PaymentTermsFactory;
+use App\Filters\PaymentTermsFilter;
+use App\Http\Requests\SignupRequest;
+use App\Requests\GroupSetting\StoreGroupSettingRequest;
+use App\Requests\GroupSetting\UpdateGroupSettingRequest;
+use App\GroupSetting;
+use App\Repositories\PaymentTermsRepository;
+use App\Requests\SearchRequest;
+use App\Settings\PaymentTerms;
+use App\Transformations\PaymentTermsTransformable;
+use App\Traits\UploadableTrait;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+/**
+ * Class GroupSettingController
+ * @package App\Http\Controllers
+ */
+class PaymentTermsControllerController extends Controller
+{
+    use DispatchesJobs;
+    use UploadableTrait;
+    use PaymentTermsTransformable;
+
+    protected PaymentTermsRepository $payment_terms_repo;
+
+    /**
+     * GroupSettingController constructor.
+     * @param PaymentTermsRepository $payment_terms_repo
+     */
+    public function __construct(PaymentTermsRepository $payment_terms_repo)
+    {
+        $this->payment_terms_repo = $payment_terms_repo;
+    }
+
+    /**
+     * @param SearchRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(SearchRequest $request)
+    {
+        $payment_terms = (new PaymentTermsFilter($this->payment_terms_repo))->filter(
+            $request,
+            auth()->user()->account_user()->account
+        );
+
+        return response()->json($payment_terms);
+    }
+
+    /**
+     * @param StoreGroupSettingRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StorePaymentTermsRequest $request)
+    {
+        $payment_terms = PaymentTermsFactory::create(auth()->user()->account_user()->account, auth()->user());
+        $payment_terms = $this->payment_terms_repo->save($request->all(), $payment_terms);
+     
+        return response()->json($this->transformPaymentTerms($payment_terms));
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(int $id)
+    {
+        return response()->json($this->transformPaymentTerms($this->payment_terms_repo->findPaymentTermsById($id)));
+    }
+
+    /**
+     * @param int $id
+     * @param UpdateGroupSettingRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(int $id, UpdatePaymentTermsRequest $request)
+    {
+        $payment_terms = $this->payment_terms_repo->findPaymentTermsById($id);
+        $payment_terms = $this->payment_terms_repo->save($request->all(), $payment_terms);
+        return response()->json($this->transformPaymentTerms($payment_terms));
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function archive(int $id)
+    {
+        $payment_terms = $this->payment_terms_repo->findPaymentTermsById($id);
+        $payment_terms->delete();
+        return response()->json([], 200);
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(int $id)
+    {
+        $payment_terms = PaymentTerms::withTrashed()->where('id', '=', $id)->first();
+        $this->payment_terms_repo->newDelete($payment_terms);
+        return response()->json([], 200);
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function restore(int $id)
+    {
+        $payment_terms = PaymentTerms::withTrashed()->where('id', '=', $id)->first();
+        $this->payment_terms_repo->restore($payment_terms);
+        return response()->json([], 200);
+    }
+}
