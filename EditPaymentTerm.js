3c3fece55d25131e38b7b6343244bbba5e3d611b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-23 21:24:35 +0100

    Create EditPaymentTerm.js

---
+++ EditPaymentTerm.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, FormGroup, Label, DropdownItem } from 'reactstrap'
+import axios from 'axios'
+import { icons, translations } from '../common/_icons'
+
+class EditPaymentTerm extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            modal: false,
+            id: this.props.payment_term.id,
+            name: this.props.payment_term.name,
+            loading: false,
+            changesMade: false,
+            errors: []
+        }
+
+        this.initialState = this.state
+        this.toggle = this.toggle.bind(this)
+        this.hasErrorFor = this.hasErrorFor.bind(this)
+        this.renderErrorFor = this.renderErrorFor.bind(this)
+    }
+
+    handleInput (e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+            changesMade: true
+        })
+    }
+
+    hasErrorFor (field) {
+        return !!this.state.errors[field]
+    }
+
+    renderErrorFor (field) {
+        if (this.hasErrorFor(field)) {
+            return (
+                <span className='invalid-feedback'>
+                    <strong>{this.state.errors[field][0]}</strong>
+                </span>
+            )
+        }
+    }
+
+    handleClick () {
+        axios.put(`/api/payment_terms/${this.state.id}`, {
+            name: this.state.name,
+            
+        })
+            .then((response) => {
+                const index = this.props.payment_terms.findIndex(payment_term => payment_term.id === this.state.id)
+                this.props.payment_terms[index] = response.data
+                this.props.action(this.props.payment_terms)
+                this.setState({ changesMade: false })
+                this.toggle()
+            })
+            .catch((error) => {
+                this.setState({
+                    errors: error.response.data.errors
+                })
+            })
+    }
+
+    toggle () {
+        if (this.state.modal && this.state.changesMade) {
+            if (window.confirm('Your changes have not been saved?')) {
+                this.setState({ ...this.initialState })
+            }
+
+            return
+        }
+
+        this.setState({
+            modal: !this.state.modal,
+            errors: []
+        })
+    }
+
+    render () {
+        return (
+            <React.Fragment>
+                <DropdownItem onClick={this.toggle}><i className={`fa ${icons.edit}`}/>Edit</DropdownItem>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>
+                        {translations.edit_group}
+                    </ModalHeader>
+                    <ModalBody>
+                        <FormGroup>
+                            <Label for="name">{translations.name} <span className="text-danger">*</span></Label>
+                            <Input className={this.hasErrorFor('name') ? 'is-invalid' : ''}
+                                value={this.state.name}
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder={translations.name} onChange={this.handleInput.bind(this)}/>
+                            {this.renderErrorFor('name')}
+                        </FormGroup>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.handleClick.bind(this)}>Add</Button>
+                        <Button color="secondary" onClick={this.toggle}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+            </React.Fragment>
+        )
+    }
+}
+
+export default EditPaymentTerm
