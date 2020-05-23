b79a47afe543044c516b4cbb9548cd3e3b74c6c7
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-23 10:05:45 +0100

    Update GroupSettingsTest.php

--- tests/Unit/GroupSettingsTest.php
+++ tests/Unit/GroupSettingsTest.php
@@ -40,9 +40,9 @@ class GroupSettingsTest extends TestCase
     /** @test */
     public function it_can_show_all_the_projects()
     {
-        $insertedproject = factory(Project::class)->create();
-        $projectRepo = new ProjectRepository(new Project);
-        $list = $projectRepo->listProjects()->toArray();
+        $insertedproject = factory(GroupSetting::class)->create();
+        $group_settings_repo = new GroupSettingRepository(new GroupSetting);
+        $list =  $group_settings_repo->listProjects()->toArray();
         $myLastElement = end($list);
         // $this->assertInstanceOf(Collection::class, $list);
         $this->assertEquals($insertedproject->toArray(), $myLastElement);
@@ -52,8 +52,8 @@ class GroupSettingsTest extends TestCase
     public function it_can_delete_the_project()
     {
         $project = factory(Project::class)->create();
-        $projectRepo = new ProjectRepository($project);
-        $deleted = $projectRepo->newDelete($project);
+        $group_settings_repo = new ProjectRepository($project);
+        $deleted = $group_settings_repo->newDelete($project);
         $this->assertTrue($deleted);
     }
 
@@ -63,9 +63,9 @@ class GroupSettingsTest extends TestCase
         $project = factory(Project::class)->create();
         $title = $this->faker->word;
         $data = ['title' => $title];
-        $projectRepo = new ProjectRepository($project);
-        $updated = $projectRepo->save($data, $project);
-        $found = $projectRepo->findProjectById($project->id);
+        $group_settings_repo = new ProjectRepository($project);
+        $updated = $group_settings_repo->save($data, $project);
+        $found = $group_settings_repo->findProjectById($project->id);
         $this->assertInstanceOf(Project::class, $updated);
         $this->assertEquals($data['title'], $found->title);
     }
@@ -74,8 +74,8 @@ class GroupSettingsTest extends TestCase
     public function it_can_show_the_project()
     {
         $project = factory(Project::class)->create();
-        $projectRepo = new ProjectRepository(new Project);
-        $found = $projectRepo->findProjectById($project->id);
+        $group_settings_repo = new ProjectRepository(new Project);
+        $found = $group_settings_repo->findProjectById($project->id);
         $this->assertInstanceOf(Project::class, $found);
         $this->assertEquals($project->name, $found->name);
     }
@@ -84,36 +84,14 @@ class GroupSettingsTest extends TestCase
     public function it_can_create_a_project()
     {
         $data = [
-            'account_id'   => $this->account->id,
-            'user_id'      => $this->user->id,
-            'title'        => $this->faker->word,
-            'description'  => $this->faker->sentence,
-            'is_completed' => 0,
+            'name'        => $this->faker->word,
         ];
 
-        $projectRepo = new ProjectRepository(new Project);
-        $factory = (new ProjectFactory())->create($this->user, $this->customer, $this->account);
-        $project = $projectRepo->save($data, $factory);
+        $group_settings_repo = new ProjectRepository(new Project);
+        $factory = (new GroupSettingFactory())->create($this->user, $this->customer, $this->account);
+        $project = $group_settings_repo->save($data, $factory);
         $this->assertInstanceOf(Project::class, $project);
         $this->assertEquals($data['title'], $project->title);
-    }
-
-    /**
-     * @codeCoverageIgnore
-     */
-    public function it_errors_creating_the_project_when_required_fields_are_not_passed()
-    {
-        $this->expectException(\Illuminate\Database\QueryException::class);
-        $product = new ProjectRepository(new Project);
-        $product->createProject([]);
-    }
-
-    /** @test */
-    public function it_errors_finding_a_project()
-    {
-        $this->expectException(\Illuminate\Database\Eloquent\ModelNotFoundException::class);
-        $category = new ProjectRepository(new Project);
-        $category->findProjectById(999);
     }
 
     public function tearDown(): void
