049d8ce032cff850c424b1633ae471584c2db78d
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-23 09:44:40 +0100

    Create PaymentTermsTransformable.php

---
+++ app/Transformations/PaymentTermsTransformable.php
@@ -0,0 +1,24 @@
+
+<?php
+
+namespace App\Transformations;
+
+use App\PaymentTerms;
+
+trait PaymentTermsTransformable
+{
+    /**
+     * @param GroupSetting $group_setting
+     * @return array
+     */
+    protected function transformPaymentTerms(PaymentTerms $payment_terms)
+    {
+        return [
+            'id'               => (int)$payment_terms->id,
+            'created_at'       => $payment_terms->created_at,
+            'deleted_at'       => $payment_terms->deleted_at,
+            'name'             => (string)$payment_terms->name ?: '',
+            'number_of_days'   => (int)$payment_terms->number_of_days ?: 1,
+        ];
+    }
+}
