93d4eecd9e47b3abf0e38769616999fcfaa19e53
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-27 19:07:23 +0100

    Create ReverseInvoicePayment.php

---
+++ app/Services/Payment/ReverseInvoicePayment.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Services\Payment;  
+
+use App\Payment;
+use App\Customer;
+
+class ReverseInvoicePayment()
+{
+    private Payment $payment;
+
+    public function __construct(Payment $payment)
+    {
+        $this->payment = $payment;
+    } 
+
+    public function run(): ?Payment
+    {
+        $this->reversePayment(); 
+        $this->updateCustomer();
+        $this->deletePayment();
+
+        return $this->payment;
+    }
+
+    private function reversePayment(): bool
+    {
+        $invoices = $this->payment->invoices;
+
+        foreach ($invoices as $invoice) {
+            if ($invoice->pivot->amount <= 0) {
+                continue;
+            }
+
+            $invoice->setStatus(Invoice::STATUS_SENT);
+            $invoice->setBalance($invoice->pivot->amount);
+            $invoice->save();
+        }
+
+        return true;
+    }
+
+    private function updateCustomer(): Customer
+    {
+        $customer = $this->payment->customer;
+        $customer->increaseBalance($this->amount);
+        $customer->increasePaidToDateAmount($this->amount * -1);
+        $customer->save();
+        return $customer;
+    }
+}
