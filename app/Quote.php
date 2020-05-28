19a603c3049c421944fb50c461946a0c16b246d7
michael-hampton <michaelhamptondesign@yahoo.com>
2020-05-28 23:36:23 +0100

    Update Quote.php

--- app/Quote.php
+++ app/Quote.php
@@ -134,10 +134,34 @@ class Quote extends Model
     }
 
     /********************** Getters and setters ************************************/
+    public function setUser(User $user)
+    {
+        $this->user_id = (int) $user->id;
+    }
+
+    public function setAccount(Account $account)
+    {
+        $this->account_id = (int) $account->id;
+    }
+
+    public function setCustomer(Customer $customer)
+    {
+        $this->customer_id = (int) $customer->id;
+    }
 
     public function setStatus(int $status)
     {
         $this->status_id = $status;
+    }
+
+    public function setBalance(float $balance)
+    {
+        $this->balance = (float) $balance;
+    }
+
+    public function setTotal(float $total)
+    {
+        $this->total = (float) $total;
     }
 
     public function setInvoiceId($invoice_id)
