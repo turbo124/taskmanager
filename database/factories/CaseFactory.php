cafae090bbb74d28313c819fe79879d678cd6194
michael-hampton <michaelhamptondesign@yahoo.com>
2020-06-03 21:36:57 +0100

    Update CaseFactory.php

--- database/factories/CaseFactory.php
+++ database/factories/CaseFactory.php
@@ -12,12 +12,16 @@ $factory->define(
         $customer = factory(\App\Customer::class)->create();
         $account = \App\Account::first();
         return [
-            'status_id'   => \App\Cases::STATUS_DRAFT,
-            'subject'     => $faker->word,
-            'message'     => $faker->sentence,
-            'account_id'  => $account->id,
-            'user_id'     => $user->id,
-            'customer_id' => $customer->id
+            'status_id'     => \App\Cases::STATUS_DRAFT,
+            'subject'       => $faker->word,
+            'message'       => $faker->sentence,
+            'private_notes' => $faker->sentence,
+            'account_id'    => $account->id,
+            'user_id'       => $user->id,
+            'customer_id'   => $customer->id,
+            'category_id'   => 1,
+            'priority_id'   => 1,
+            'due_date'      => \Carbon\Carbon::today()->addDays(5),
         ];
     }
 );
