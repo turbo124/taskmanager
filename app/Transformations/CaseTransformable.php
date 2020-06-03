2f01fb250dcc36a95ba9740707fdeb22ca40b436
michael-hampton <michaelhamptondesign@yahoo.com>
2020-06-03 20:49:18 +0100

    Update CaseTransformable.php

--- app/Transformations/CaseTransformable.php
+++ app/Transformations/CaseTransformable.php
@@ -16,16 +16,20 @@ trait CaseTransformable
     public function transform(Cases $cases)
     {
         return [
-            'id'          => (int)$cases->id,
-            'message'     => $cases->message,
-            'subject'     => $cases->subject,
-            'account_id'  => (int)$cases->account_id,
-            'customer_id' => (int)$cases->customer_id,
-            'user_id'     => (int)$cases->user_id,
-            'status_id'   => (int)$cases->status_id,
-            'updated_at'  => $cases->updated_at,
-            'created_at'  => $cases->created_at,
-            'is_deleted'  => (bool)$cases->is_deleted
+            'id'            => (int)$cases->id,
+            'message'       => $cases->message,
+            'subject'       => $cases->subject,
+            'private_notes' => $cases->private_notes,
+            'due_date'      => $cases->due_date,
+            'account_id'    => (int)$cases->account_id,
+            'customer_id'   => (int)$cases->customer_id,
+            'user_id'       => (int)$cases->user_id,
+            'status_id'     => (int)$cases->status_id,
+            'category_id'   => (int)$cases->category_id,
+            'priority_id'   => (int)$cases->priority_id,
+            'updated_at'    => $cases->updated_at,
+            'created_at'    => $cases->created_at,
+            'is_deleted'    => (bool)$cases->is_deleted
         ];
     }
 }
