c29b521b5fccb34966216cf24beed924b7d2adaf
michael-hampton <michaelhamptondesign@yahoo.com>
2020-06-11 18:20:36 +0100

    Update InvoiceRefund.php

--- app/Helpers/Refund/InvoiceRefund.php
+++ app/Helpers/Refund/InvoiceRefund.php
@@ -11,22 +11,25 @@ use App\Repositories\CreditRepository;
 
 class InvoiceRefund extends BaseRefund
 {
+    private array $payment_invoices;
 
     /**
      * InvoiceRefund constructor.
      * @param Payment $payment
      * @param array $data
      * @param CreditRepository $credit_repository
+     * @param array $payment_invoices
      */
-    public function __construct(Payment $payment, array $data, CreditRepository $credit_repository)
+    public function __construct(Payment $payment, array $data, CreditRepository $credit_repository, array $payment_invoices)
     {
         parent::__construct($payment, $data, $credit_repository);
+        $this->payment_invoices = $payment_invoices;
     }
 
     /**
      * @return Payment
      */
-    public function refund($payment_invoices)
+    public function refund($objCreditRefund = null)
     {
         $ids = array_column($payment_invoices, 'invoice_id');
         $invoices = Invoice::whereIn('id', $ids)->get()->keyBy('id');
