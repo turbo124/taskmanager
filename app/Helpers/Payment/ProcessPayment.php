819479e29026664684840d36edee433d453ad12b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-06-11 19:26:25 +0100

    Update ProcessPayment.php

--- app/Helpers/Payment/ProcessPayment.php
+++ app/Helpers/Payment/ProcessPayment.php
@@ -1,1 +1,22 @@
+<?php
 
+
+namespace App\Helpers\Payment;
+
+
+use App\Payment;
+use App\Repositories\PaymentRepository;
+
+class ProcessPayment
+{
+
+    /**
+     * @param Payment $payment
+     * @param array $data
+     * @param PaymentRepository $payment_repo
+     * @return Payment|bool
+     */
+    public function process(array $data, PaymentRepository $payment_repo)
+    {
+    }
+}
