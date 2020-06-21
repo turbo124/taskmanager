dce89c44733363abf618712978de0bef353497a1
michael-hampton <michaelhamptondesign@yahoo.com>
2020-06-21 13:23:47 +0100

    Update Transaction.js

--- resources/assets/js/components/customers/Transaction.js
+++ resources/assets/js/components/customers/Transaction.js
@@ -9,7 +9,7 @@ export default function Transaction (props) {
         return (<dl key={index} className="row border-bottom">
             <dt className="col-sm-2">{transaction.entity_name}</dt>
             <dt className="col-sm-3">{<FormatDate date={transaction.created_at}/>}</dt>
-            <dd className="col-sm-2">{<FormatMoney amount={transaction.amount}/>}<br/><FormatMoney
+            <dd className="col-sm-2">{<FormatMoney className={text_color} amount={transaction.amount}/>}<br/><FormatMoney
                 amount={transaction.updated_balance}/>
             </dd>
             <dd className="col-sm-5">{transaction.notes}</dd>
