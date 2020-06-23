d1c9259b6333122e1036dcbf2bc7f97ae7258e74
michael-hampton <michaelhamptondesign@yahoo.com>
2020-06-23 18:48:56 +0100

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/api/tasks/deal',
+        '/api/customer/register',
         'api/lead',
         '/api/invoice/download',
         '/api/recurring-invoice/cancel',
