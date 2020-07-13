c406205ebe3966e4b1b01466a5c022a5abbab589
michael-hampton <michaelhamptondesign@yahoo.com>
2020-07-13 18:42:19 +0100

    Update LineItemEditor.js

--- resources/assets/js/components/common/LineItemEditor.js
+++ resources/assets/js/components/common/LineItemEditor.js
@@ -165,7 +165,13 @@ class LineItemEditor extends Component {
         }
 
         if (this.props.invoice.gateway_fee && this.props.invoice.gateway_fee > 0) {
-            total += this.props.invoice.gateway_fee
+            let gateway_amount = this.props.invoice.gateway_fee
+            
+            if (this.props.invoice.gateway_percentage === true) {
+                gateway_amount = total * this.props.invoice.gateway_fee / 100
+            }
+
+            total += gateway_amount
         }
 
         if (this.props.invoice.total_custom_tax && this.props.invoice.total_custom_tax > 0) {
