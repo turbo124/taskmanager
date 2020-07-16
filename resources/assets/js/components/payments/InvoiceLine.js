ef545a8a0177f47b064029472d0d7c0831069bb8
michael-hampton <michaelhamptondesign@yahoo.com>
2020-07-16 19:33:05 +0100

    Update InvoiceLine.js

--- resources/assets/js/components/payments/InvoiceLine.js
+++ resources/assets/js/components/payments/InvoiceLine.js
@@ -60,6 +60,38 @@ export default class InvoiceLine extends Component {
             }
         }
 
+        if (name === 'credit_id' || (e.target.dataset.credit && e.target.dataset.credit.length)) {
+            const credit_id = e.target.dataset.credit && e.target.dataset.credit.length ? e.target.dataset.credit : e.target.value
+            const credit = this.paymentModel.getCredit(credit_id)
+
+            if (!credit) {
+                return
+            }
+
+            let credit_total = e.target.dataset.credit && e.target.dataset.credit.length && name === 'amount' ? parseFloat(e.target.value) : parseFloat(credit.total)
+            let refunded_amount = 0
+
+            if (this.props.paymentables && this.props.paymentables.length > 0) {
+                refunded_amount = this.paymentModel.calculateRefundedAmount(this.props.paymentables)
+            }
+
+            if ((refunded_amount + credit_total) > credit.total) {
+                const amount_remaining = credit.total - refunded_amount
+                invoice_total = amount_remaining
+                manual_update = true
+            }
+
+            this.props.customerChange(credit.customer_id)
+            lines[idx].amount = parseFloat(credit_total)
+            lines[idx].credit_id = credit_id
+
+            if (this.props.allCredits && this.props.allCredits.length === 1) {
+                amount = credit_total
+            } else {
+                amount = this.state.amount + parseFloat(credit_total)
+            }
+        }
+
         if (!manual_update) {
             lines[e.target.dataset.id][e.target.name] = e.target.value
         }
@@ -75,13 +107,14 @@ export default class InvoiceLine extends Component {
 
     addLine (e) {
         const allowedInvoices = this.paymentModel.getInvoicesByStatus(this.props.status)
+        const allowedCredits = this.paymentModel.getCreditsByStatus(2)
 
-        if (this.state.lines.length >= allowedInvoices.length) {
+        if (this.state.lines.length >= (allowedInvoices.length + allowedCredits.length)) {
             return
         }
 
         this.setState((prevState) => ({
-            lines: [...prevState.lines, { invoice_id: null, amount: 0 }]
+            lines: [...prevState.lines, { invoice_id: null, amount: 0, credit_id: null }]
         }), () => this.props.onChange(this.state.lines))
     }
 
@@ -101,9 +134,10 @@ export default class InvoiceLine extends Component {
         const { lines } = this.state
         const status = this.props.status ? this.props.status : null
         const invoices = this.props.allInvoices ? this.props.allInvoices : []
+        const credits = this.props.allCredits ? this.props.allCredits : []
         return (
             <form>
-                <InvoiceLineInputs invoices={invoices} status={status} errors={this.props.errors}
+                <InvoiceLineInputs invoices={invoices} credits={credits} status={status} errors={this.props.errors}
                     onChange={this.handleChange} lines={lines}
                     removeLine={this.removeLine}
                     addLine={this.addLine}/>
