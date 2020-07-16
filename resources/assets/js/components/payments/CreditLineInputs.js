c404e08520e33e30dedf3db907ce0c0d79398e1c
michael-hampton <michaelhamptondesign@yahoo.com>
2020-07-16 20:24:34 +0100

    Create CreditLineInputs.js

---
+++ resources/assets/js/components/payments/CreditLineInputs.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Col, Row, Button, FormGroup, Label, Input } from 'reactstrap'
+import CreditDropdown from '../common/CreditDropdown'
+import { translations } from '../common/_translations'
+
+const CreditLineInputs = (props) => {
+    return (
+        props.lines.map((val, idx) => {
+            return (
+                <div key={idx}>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="exampleEmail">{translations.invoice}</Label>
+                                <CreditDropdown
+                                    credits={props.credits}
+                                    data_id={idx}
+                                    invoice_id={props.lines[idx].credit_id}
+                                    status={props.status}
+                                    handleInputChanges={props.onChange}
+                                    name="credit_id"
+                                    errors={props.errors}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for="examplePassword">{translations.amount}</Label>
+                                <Input type="text"
+                                    data-invoice={props.credits.length === 1 ? props.credits[0].id : null}
+                                    data-id={idx}
+                                    onChange={props.onChange}
+                                    value={props.lines[idx].amount}
+                                    autoFocus={(props.credits && props.credits.length === 1) || idx === 0}
+                                    name="amount"
+                                />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    {props.credits.length > 1 &&
+                        <React.Fragment>
+                            <Button color="danger" onClick={() => props.removeCredit(idx)}>
+                                {translations.remove}
+                            </Button>
+                            <Button color="primary" onClick={() => props.addCredit(idx)}>
+                                {translations.add}
+                            </Button>
+                        </React.Fragment>
+                    }
+                </div>
+            )
+        })
+    )
+}
+export default CreditLineInputs
