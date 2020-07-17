9b873cc5921b3a39c6685363eceb4b6d8e170432
michael-hampton <michaelhamptondesign@yahoo.com>
2020-07-17 22:07:25 +0100

    Create InvoiceWasArchived.php

---
+++ app/Events/Invoice/InvoiceWasArchived.php
@@ -0,0 +1,32 @@
+
+<?php
+
+namespace App\Events\Invoice;
+
+use App\Invoice;
+use App\Traits\SendSubscription;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Class InvoiceWasArchived.
+ */
+class InvoiceWasArchived
+{
+    use SerializesModels;
+    use SendSubscription;
+
+    /**
+     * @var Invoice
+     */
+    public Invoice $invoice;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Invoice $invoice
+     */
+    public function __construct(Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+    }
+}
