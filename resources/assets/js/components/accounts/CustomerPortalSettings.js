874bc7735f2b7aa9ab92b02d0d46e288eca33cb4
michael-hampton <michaelhamptondesign@yahoo.com>
2020-07-31 21:38:54 +0100

    Update CustomerPortalSettings.js

--- resources/assets/js/components/accounts/CustomerPortalSettings.js
+++ resources/assets/js/components/accounts/CustomerPortalSettings.js
@@ -164,7 +164,7 @@ export default class CustomerPortalSettings extends Component {
                                 <a className="pull-right pr-3" onClick={this.handleSubmit}>{translations.save}</a>
                             </div>
 
-                            <Nav tabs className="setting-tabs disable-scrollbars">
+                            <Nav tabs className="nav-justified setting-tabs disable-scrollbars">
                                 <NavItem>
                                     <NavLink
                                         className={this.state.activeTab === '1' ? 'active' : ''}
