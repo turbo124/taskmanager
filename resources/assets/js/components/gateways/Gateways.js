bb32d0f46f9ec89f50af49c35ec1c0072fbec59a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 20:03:03 +0100

    Update Gateways.js

--- resources/assets/js/components/gateways/Gateways.js
+++ resources/assets/js/components/gateways/Gateways.js
@@ -69,6 +69,9 @@ export default class Gateways extends Component {
         const { searchText, status, start_date, end_date, error } = this.state.filters
         const { view, gateways } = this.state
         const fetchUrl = `/api/company_gateways?search_term=${searchText} `
+        const margin_class = Object.prototype.hasOwnProperty.call(localStorage, 'datatable_collapsed') && localStorage.getItem('datatable_collapsed) === true
+            ? 'fixed-margin-datatable-collapsed'
+            : 'fixed-margin-datatable fixed-margin-datatable-mobile'
 
         return (
             <React.Fragment>
@@ -96,7 +99,7 @@ export default class Gateways extends Component {
                 </Snackbar>
                 }
 
-                <div className="fixed-margin-datatable fixed-margin-datatable-mobile">
+                <div className={margin_class}>
                     <Card>
                         <CardBody>
                             <DataTable
