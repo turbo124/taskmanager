9f5e75d8ff17b2e89685886f0e67c28bf42bdffa
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:10:15 +0100

    Update ProductFilters.js

--- resources/assets/js/components/products/ProductFilters.js
+++ resources/assets/js/components/products/ProductFilters.js
@@ -114,6 +114,6 @@ export default class ProductFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
