a9bd349d4592d7fa8de7255edbde7430e2c1c913
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:11:54 +0100

    Update RecurringQuoteFilters.js

--- resources/assets/js/components/recurringQuotes/RecurringQuoteFilters.js
+++ resources/assets/js/components/recurringQuotes/RecurringQuoteFilters.js
@@ -106,6 +106,6 @@ export default class RecurringQuoteFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
