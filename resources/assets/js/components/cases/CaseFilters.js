bff321a2dc16cdcb2bfa990d6facf6493218b003
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:03:24 +0100

    Update CaseFilters.js

--- resources/assets/js/components/cases/CaseFilters.js
+++ resources/assets/js/components/cases/CaseFilters.js
@@ -120,6 +120,6 @@ export default class CaseFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
