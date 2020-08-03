480750fcc3725678a5f4459e8b38254ed598e456
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 21:55:20 +0100

    Update QuoteFilters.js

--- resources/assets/js/components/quotes/QuoteFilters.js
+++ resources/assets/js/components/quotes/QuoteFilters.js
@@ -15,6 +15,7 @@ export default class QuoteFilters extends Component {
     constructor (props) {
         super(props)
         this.state = {
+            isOpen: false,
             dropdownButtonActions: ['download'],
             filters: {
                 status_id: 'active',
@@ -54,6 +55,10 @@ export default class QuoteFilters extends Component {
 
         this.getFilters = this.getFilters.bind(this)
         this.filterInvoices = this.filterInvoices.bind(this)
+    }
+
+    setFilterOpen(isOpen) {
+        this.setState({isOpen: isOpen})
     }
 
     filterInvoices (event) {
