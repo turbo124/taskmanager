725b1530541f4186d85aca54ed259de462e6adea
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:06:33 +0100

    Update ProjectFilters.js

--- resources/assets/js/components/forms/ProjectFilters.js
+++ resources/assets/js/components/forms/ProjectFilters.js
@@ -104,6 +104,6 @@ export default class ProjectFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
