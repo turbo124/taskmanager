c1abe02f8653bd60a1cee17e61940ef0b3a24ca3
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 20:40:20 +0100

    Update Subscriptions.js

--- resources/assets/js/components/subscriptions/Subscriptions.js
+++ resources/assets/js/components/subscriptions/Subscriptions.js
@@ -79,6 +79,10 @@ export default class Subscriptions extends Component {
             })
     }
 
+    handleClose () {
+        this.setState({ error: '' })
+    }
+
     render () {
         const { searchText, status, start_date, end_date } = this.state.filters
         const { view, subscriptions, error } = this.state
