66c626ce6d96c7474ef5db05e82d5031f2776817
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:08:58 +0100

    Update OrderFilters.js

--- resources/assets/js/components/orders/OrderFilters.js
+++ resources/assets/js/components/orders/OrderFilters.js
@@ -136,6 +136,6 @@ export default class OrderFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
