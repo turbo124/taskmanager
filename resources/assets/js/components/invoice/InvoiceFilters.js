29c22cb4fbe3b9ecd1e915382ed7e15a8ce2d610
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:07:52 +0100

    Update InvoiceFilters.js

--- resources/assets/js/components/invoice/InvoiceFilters.js
+++ resources/assets/js/components/invoice/InvoiceFilters.js
@@ -137,6 +137,6 @@ export default class InvoiceFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
