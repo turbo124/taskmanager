8be76e59dbe57b539db3b4fb5ed9d30f1a3afd62
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:09:22 +0100

    Update PaymentTermFilters.js

--- resources/assets/js/components/paymentTerms/PaymentTermFilters.js
+++ resources/assets/js/components/paymentTerms/PaymentTermFilters.js
@@ -83,6 +83,6 @@ export default class PaymentTermFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
