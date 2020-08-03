a6bf759b0fa7fbe9e6b0be0a7c4d46dc70a31e17
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:11:05 +0100

    Update QuoteFilters.js

--- resources/assets/js/components/quotes/QuoteFilters.js
+++ resources/assets/js/components/quotes/QuoteFilters.js
@@ -134,6 +134,6 @@ export default class QuoteFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
