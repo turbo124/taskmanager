212c09235f7dd435ca582d8a654f06a249e83de2
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:07:00 +0100

    Update TaskFilters.js

--- resources/assets/js/components/forms/TaskFilters.js
+++ resources/assets/js/components/forms/TaskFilters.js
@@ -127,6 +127,6 @@ export default class TaskFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
