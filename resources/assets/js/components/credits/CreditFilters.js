02373ee6f807ed106f582256a1056ef75dce27c7
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 22:04:49 +0100

    Update CreditFilters.js

--- resources/assets/js/components/credits/CreditFilters.js
+++ resources/assets/js/components/credits/CreditFilters.js
@@ -123,6 +123,6 @@ export default class CreditFilters extends Component {
     render () {
         const filters = this.getFilters()
 
-        return (<FilterTile filters={filters}/>)
+        return (<FilterTile setFilterOpen={this.setFilterOpen.bind(this)} filters={filters}/>)
     }
 }
