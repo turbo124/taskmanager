a984a2a51b1a2d4de4fa8b454dfc0ca0a4e76fc0
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-03 19:48:13 +0100

    Update Attributes.js

--- resources/assets/js/components/attributes/Attributes.js
+++ resources/assets/js/components/attributes/Attributes.js
@@ -84,6 +84,9 @@ export default class Attributes extends Component {
         const { searchText, status, start_date, end_date } = this.state.filters
         const { view, attributes, error } = this.state
         const fetchUrl = `/api/attributes?search_term=${searchText}&status=${status}&start_date=${start_date}&end_date=${end_date} `
+        const margin_class = Object.prototype.hasOwnProperty.call(localStorage, 'datatable_collapsed') && localStorage.getItem('datatable_collapsed) === true
+            ? 'fixed-margin-datatable-collapsed'
+            : 'fixed-margin-datatable fixed-margin-datatable-mobile'
 
         return (
             <React.Fragment>
@@ -111,7 +114,7 @@ export default class Attributes extends Component {
                 </Snackbar>
                 }
 
-                <div className="fixed-margin-datatable fixed-margin-datatable-mobile">
+                <div className={margin_class}>
                     <Card>
                         <CardBody>
                             <DataTable
