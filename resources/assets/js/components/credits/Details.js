9c82106a409a7ddfa4201d84350d449137448f28
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-18 22:31:26 +0100

    Update Details.js

--- resources/assets/js/components/credits/Details.js
+++ resources/assets/js/components/credits/Details.js
@@ -82,6 +82,18 @@ export default class Details extends React.Component {
                     />
                     {this.renderErrorFor('number')}
                 </FormGroup>
+
+                <FormGroup>
+                    <Label>{translations.number}</Label>
+                    <Input className={this.hasErrorFor('number') ? 'form-control is-invalid' : 'form-control'}
+                        value={this.props.credit.return_to_stock}
+                        type='checkbox'
+                        name='return_to_stock'
+                        id='return_to_stock'
+                        onChange={this.props.handleInput}
+                    />
+                    {this.renderErrorFor('number')}
+                </FormGroup>
             </CardBody>
         </Card>
         )
