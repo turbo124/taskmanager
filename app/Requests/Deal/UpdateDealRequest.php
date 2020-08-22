5719232e51986d40cc8be6a90216f7d4fd9b8dc6
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-22 14:18:43 +0100

    Create UpdateDealRequest.php

---
+++ app/Requests/Deal/UpdateDealRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Requests\Deal;
+
+use App\Repositories\Base\BaseFormRequest;
+
+class UpdateDealRequest extends BaseFormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'valued_at'    => 'nullable|string',
+            'rating'       => 'nullable|numeric',
+            'customer_id'  => 'nullable|numeric',
+            'title'        => 'required',
+          
+        ];
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'title.required'        => 'Title is required!',
+            'content.required'      => 'Content is required!',
+            'contributors.required' => 'Contributors is required!',
+            'due_date.required'     => 'Due date is required!',
+        ];
+    }
+
+}
