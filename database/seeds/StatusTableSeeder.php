6c9b025dad115410e8422e5cfd9196581910164b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-22 15:27:36 +0100

    Update StatusTableSeeder.php

--- database/seeds/StatusTableSeeder.php
+++ database/seeds/StatusTableSeeder.php
@@ -45,6 +45,88 @@ class StatusTableSeeder extends Seeder {
             'task_type' => 1,
                 ]
         );
+
+        // deals
+        DB::table('task_statuses')->insert([
+            'title' => 'Prospecting',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+ 
+        DB::table('task_statuses')->insert([
+            'title' => 'Qualification',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+
+       DB::table('task_statuses')->insert([
+            'title' => 'Needs Analysis',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+
+        DB::table('task_statuses')->insert([
+            'title' => 'Value Propsition',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+
+        DB::table('task_statuses')->insert([
+            'title' => 'Identified Decision Makers',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+
+        DB::table('task_statuses')->insert([
+            'title' => 'Perception Analysis',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+
+        DB::table('task_statuses')->insert([
+            'title' => 'Proposal Price\Quote',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+
+        DB::table('task_statuses')->insert([
+            'title' => 'Closed Won',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
+
+        DB::table('task_statuses')->insert([
+            'title' => 'Closed Lost',
+            'description' => 'You can do what you want to do with this column',
+            'icon' => 'fa-check',
+            'is_active' => 1,
+            'task_type' => 2,
+                ]
+        );
     }
 
 }
