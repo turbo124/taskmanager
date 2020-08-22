fd78efc6a119f936ebd2707f1778e66a197fb8be
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-22 13:51:35 +0100

    Create DealPresenter.js

---
+++ resources/assets/js/components/presenters/DealPresenter.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import ViewTask from '../forms/ViewTask'
+
+export default function TaskPresenter (props) {
+    const { field, entity } = props
+
+    switch (field) {
+       default:
+            return <td onClick={() => props.toggleViewedEntity(entity, entity.title)} key={field}
+                data-label={field}>{entity[field]}</td>
+    }
+}
