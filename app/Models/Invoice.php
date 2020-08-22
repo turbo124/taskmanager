80251fbe570ee3b39307ac13e2ba7538a53ef9d1
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-22 12:43:44 +0100

    Update Invoice.php

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -93,9 +93,15 @@ class Invoice extends Model
     const STATUS_PAID = 3;
     const STATUS_CANCELLED = 5;
     const STATUS_REVERSED = 6;
+    
+    const PRODUCT_TYPE = 1;
+    const COMMISSION_TYPE = 2;
+    const TASK_TYPE = 3;
     const LATE_FEE_TYPE = 4;
     const SUBSCRIPTION_TYPE = 5;
-    const COMMISSION_TYPE = 2;
+    const EXPENSE_TYPE = 6;
+    const GATEWAY_FEE_TYPE = 7;
+   
 
     public function service(): InvoiceService
     {
