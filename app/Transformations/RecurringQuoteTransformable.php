7da321683d5f2fae46c5193f265ecee6c31398d0
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-23 16:14:44 +0100

    Update RecurringQuoteTransformable.php

--- app/Transformations/RecurringQuoteTransformable.php
+++ app/Transformations/RecurringQuoteTransformable.php
@@ -50,4 +50,53 @@ trait RecurringQuoteTransformable
         ];
     }
 
+    
+
+    /**
+     * @param $invitations
+     * @return array
+     */
+    private function transformRecurringQuoteInvitations($invitations)
+    {
+        if (empty($invitations)) {
+            return [];
+        }
+
+        return $invitations->map(
+            function (InvoiceInvitation $invitation) {
+                return (new InvoiceInvitationTransformable())->transformInvoiceInvitation($invitation);
+            }
+        )->all();
+    }
+
+    /**
+     * @param $invitations
+     * @return array
+     */
+    private function transformEmails($emails)
+    {
+        if ($emails->count() === 0) {
+            return [];
+        }
+
+        return $emails->map(
+            function (Email $email) {
+                return (new EmailTransformable())->transformEmail($email);
+            }
+        )->all();
+    }
+
+    public function transformAuditsForRecurringQuote($audits)
+    {
+        if (empty($audits)) {
+            return [];
+        }
+
+        return $audits->map(
+            function (Audit $audit) {
+                return (new AuditTransformable)->transformAudit($audit);
+            }
+        )->all();
+    }
+
 }
