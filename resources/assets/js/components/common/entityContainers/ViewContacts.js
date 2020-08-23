e7f065140d443c512cf10c3d881c398546dccb63
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-23 14:29:46 +0100

    Create ViewContacts.js

---
+++ resources/assets/js/components/common/entityContainers/ViewContacts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ListGroup, ListGroupItem, ListGroupItemHeading } from 'reactstrap'
+
+export default function ViewContacts (props) {
+    const listClass = !Object.prototype.hasOwnProperty.call(localStorage, 'dark_theme') || (localStorage.getItem('dark_theme') && localStorage.getItem('dark_theme') === 'true') ? 'list-group-item-dark' : ''
+
+    return <ListGroup className="mt-4 col-12">
+        <ListGroupItem className={listClass}>
+            <ListGroupItemHeading><i
+                className={`fa ${props.icon} mr-4`}/>{`${props.entity} > ${props.title}`}
+            </ListGroupItemHeading>
+        </ListGroupItem>
+    </ListGroup>
+}
