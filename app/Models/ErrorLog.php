5596f382ce1218d3d049bf26098e05a856500786
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-28 21:18:11 +0100

    Create ErrorLog.php

---
+++ app/Models/ErrorLog.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Email extends Authenticatable
+{
+    use Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'data',
+        'error_type',
+        'error_result',
+        'entity',
+        'entity_id',
+        'account_id',
+        'user_id',
+        'customer_id',
+       
+    ];
+
+    protected $casts = [
+        'data' => 'object'
+    ];
+
+     /**
+     * type
+     */
+    const PAYMENT_FAILURE = 'payment_failure';
+   
+    /**
+     * result
+     */
+    const SUCCESS = 'success';
+    const NEUTRAL = 'neutral';
+    const FAILURE = 'failure';
+
+    protected $table = 'error_log'
+}
