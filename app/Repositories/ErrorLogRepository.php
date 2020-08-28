ad00518f4d576f87f358b0247268994621340835
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-28 21:10:04 +0100

    Create ErrorLogRepository.php

---
+++ app/Repositories/ErrorLogRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\ErrorLog;
+use App\Models\User;
+use App\Repositories\Base\BaseRepository;
+use Exception;
+use Illuminate\Support\Collection;
+
+class ErrorLogRepository extends BaseRepository
+{
+
+    /**
+     * EmailRepository constructor.
+     * @param Email $email
+     */
+    public function __construct(ErrorLog $error_log)
+    {
+        parent::__construct($error_log);
+        $this->model = $error_log;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return User
+     * @throws Exception
+     */
+    public function findErrorLogById(int $id): ErrorLog
+    {
+        return $this->findOneOrFail($id);
+    }
+
+    /**
+     * @param array $columns
+     * @param string $orderBy
+     * @param string $sortBy
+     *
+     * @return Collection
+     */
+    public function listErrorLogs($columns = array('*'), string $orderBy = 'id', string $sortBy = 'asc'): Collection
+    {
+        return $this->all($columns, $orderBy, $sortBy);
+    }
+
+
+    /**
+     * @param array $data
+     * @param Email $event
+     */
+    public function save(array $data): ?ErrorLog
+    {
+        $error_log = new ErrorLog();
+        $error_log->fill($data);
+        $error_log->save();
+
+
+        return $error_log->fresh();
+    }
+}
