aa2115f5698fb26ae71ef8355dfe86d3d44bff6d
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-29 11:53:06 +0100

    Update ErrorLog.js

--- resources/assets/js/components/customers/view/ErrorLog.js
+++ resources/assets/js/components/customers/view/ErrorLog.js
@@ -6,28 +6,27 @@ import { getEntityIcon } from '../../common/_icons'
 import { translations } from '../../common/_translations'
 
 export default function ErrorLog (props) {
-    const transactions = props.transactions.length ? props.transactions.map((transaction, index) => {
-        const amount = transaction.amount
-        const text_color = amount.toString().includes('-') ? 'danger' : 'success'
-
-        return (<ListGroupItem key={index}
-            className="list-group-item-dark list-group-item-action flex-column align-items-start">
-            <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">
-                    <i className={`fa ${getEntityIcon(transaction.entity_name)} mr-4`}/>
-                    {translations[transaction.entity_name.toLowerCase()]} > {transaction.entity_number} </h5>
-                <FormatMoney className="lead mb-1" amount={transaction.updated_balance}/>
-            </div>
-
-            <div className="d-flex w-100 justify-content-between">
-                <span style={{ fontSize: '16px' }} className="text-muted"><FormatDate date={transaction.created_at}
-                    with_time={true}/></span>
-                <Badge color={text_color}><FormatMoney amount={amount}/></Badge>
-            </div>
-        </ListGroupItem>)
+    const isOpen = false
+   
+    const error_logs = props.error_logs.length ? props.error_logs.map((error_log, index) => {
+        const errorLogModel = new ErrorLogModel(error_log)
+       return (
+           <ListGroupItem>
+               <a className="btn collapsed" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="true" aria-controls="collapseExample1">
+                   <div className="d-flex w-100 justify-content-between">
+                       <h5 className="mb-1">heading here</h5>
+                       <span>icon here</span>
+                   </div>
+                   <p class="mb-1">subheading here</p>
+               </a>
+               <Collapse isOpen={isOpen}>
+        
+               </Collapse>
+           </ListGroupItem>
+       )
     }) : null
 
     return <ListGroup>
-        {transactions}
+        {error_logs}
     </ListGroup>
 }
