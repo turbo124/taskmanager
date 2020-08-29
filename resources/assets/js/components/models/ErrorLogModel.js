4d147d1208a3b35446d9b1d99382235005447604
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-29 12:27:04 +0100

    Update and rename SystemLogModel.js to ErrorLogModel.js

---
+++ resources/assets/js/components/models/ErrorLogModel.js
@@ -0,0 +1,76 @@
+import axios from 'axios'
+import BaseModel from './BaseModel'
+
+export default class SystemLogModel extends BaseModel {
+    constructor (data = null) {
+        super()
+
+        this._fields = {
+            data: '',
+            error_type: '',
+            error_result: '',
+            entity: '',
+            entity_id: '',
+            account_id: '',
+            user_id: '',
+            customer_id: ''
+        }
+
+        if (data !== null) {
+            this._fields = { ...this.fields, ...data }
+        }
+
+        this.PAYMENT = 'payment'
+        this.EMAIL = 'email'
+
+        this.SUCCESS = 'success'
+        this.NEUTRAL = 'neutral'
+        this.FAILURE = 'failure'
+
+        this.ENTITY_STRIPE = 'stripe'
+        this.ENTITY_AUTHORIZE = 'authorize'
+        this.ENTITY_PAYPAL = 'paypal'
+    }
+
+    get fields () {
+        return this._fields
+    }
+
+    get category {
+        switch (this.fields.error_type) {
+            case this.PAYMENT_FAILURE:
+                return 'payment';
+            case this.EMAIL:
+                return 'email';
+        }
+
+        return '';
+    }
+
+    get event {
+        switch (this.fields.error_result) {
+            case this.NEUTRAL:
+                return '';
+            case this.SUCCESS:
+                return 'error_log_success';
+            case this.FAILURE:
+                return 'error_log_failure';
+           
+        }
+
+        return '';
+    }
+
+    get entity {
+        switch (this.fields.entity) {
+            case this.ENTITY_PAYPAL:
+                return 'paypal';
+            case this.ENTITY_STRIPE:
+                return 'stripe';
+            case this.ENTITY_AUTHORIZE:
+                return 'authorize'
+        }
+
+    return '';
+  }
+}
