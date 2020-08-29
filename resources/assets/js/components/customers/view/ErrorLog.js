65dcb43f5dcf30f2c1d14a51bb107a1476c73fb7
michael-hampton <michaelhamptondesign@yahoo.com>
2020-08-29 12:02:46 +0100

    Update ErrorLog.js

--- resources/assets/js/components/customers/view/ErrorLog.js
+++ resources/assets/js/components/customers/view/ErrorLog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import FormatMoney from '../../common/FormatMoney'
 import FormatDate from '../../common/FormatDate'
-import { Badge, ListGroup, ListGroupItem } from 'reactstrap'
+import { Badge, ListGroup, ListGroupItem, UncontrolledCollapse } from 'reactstrap'
 import { getEntityIcon } from '../../common/_icons'
 import { translations } from '../../common/_translations'
 
@@ -12,16 +12,16 @@ export default function ErrorLog (props) {
         const errorLogModel = new ErrorLogModel(error_log)
        return (
            <ListGroupItem>
-               <a className="btn collapsed" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="true" aria-controls="collapseExample1">
+               <a id="toggler" className="btn collapsed" data-toggle="collapse" href="#collapseExample1">
                    <div className="d-flex w-100 justify-content-between">
                        <h5 className="mb-1">heading here</h5>
                        <span>icon here</span>
                    </div>
                    <p class="mb-1">subheading here</p>
                </a>
-               <Collapse isOpen={isOpen}>
-        
-               </Collapse>
+                <UncontrolledCollapse toggler="#toggler">
+                    Json viewer here
+               </UncontrolledCollapse>
            </ListGroupItem>
        )
     }) : null
