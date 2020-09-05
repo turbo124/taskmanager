05a0ab4443c01f748f68db2f2a80eb8e5d7d49a9
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-05 12:13:08 +0100

    Create UpdatePurchaseOrderRequest.php

---
+++ app/Requests/PurchaseOrder/UpdatePurchaseOrderRequest.php
@@ -0,0 +1,62 @@
+
+<?php
+
+namespace App\Requests\Quote;
+
+use App\Settings\LineItemSettings;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePurchaseOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'company_id'    => 'required',
+            'date'           => 'required',
+            'due_date'       => 'required',
+            'discount_total' => 'required',
+            'sub_total'      => 'required',
+            'total'          => 'required',
+            'tax_total'      => 'required',
+            'line_items'     => 'required|array',
+            'number'         => 'nullable|unique:purchase_orders,number,' . $this->company_id . ',id,account_id,' . $this->account_id,
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $input = $this->all();
+        $input['line_items'] = isset($input['line_items']) ? (new LineItemSettings)->save($input['line_items']) : [];
+
+        $this->replace($input);
+    }
+
+    /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'comment.required' => 'You must enter a comment!',
+            'task_id.required' => 'There was an unexpected error!',
+            'user_id.required' => 'There was an unexpected error!',
+        ];
+    }
+}
