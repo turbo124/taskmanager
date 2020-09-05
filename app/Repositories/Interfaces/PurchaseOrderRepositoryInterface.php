7c54b1afe7fd328def61393c9e7768c8fee8752d
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-05 11:35:14 +0100

    Create PurchaseOrderRepositoryInterface.php

---
+++ app/Repositories/Interfaces/PurchaseOrderRepositoryInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\Models\Account;
+use App\Models\PurchaseOrder;
+use App\Repositories\Base\BaseRepositoryInterface;
+use App\Requests\SearchRequest;
+
+interface PurchaseOrderRepositoryInterface extends BaseRepositoryInterface
+{
+
+    /**
+     * @param int $id
+     * @return PurchaseOrder
+     */
+    public function findPurchaseOrderById(int $id): PurchaseOrder;
+
+    /**
+     * @param SearchRequest $search_request
+     * @param Account $account
+     * @return mixed
+     */
+    public function getAll(SearchRequest $search_request, Account $account);
+
+    /**
+     * @param array $data
+     * @param PurchaseOrder $quote
+     * @return PurchaseOrder|null
+     */
+    public function createPurchaseOrder(array $data, PurchaseOrder $po): ?PurchaseOrder;
+
+    /**
+     * @param array $data
+     * @param PurchaseOrder $po
+     * @return PurchaseOrder|null
+     */
+    public function updatePurchaseOrder(array $data, PurchaseOrder $po): ?PurchaseOrder;
+
+    /**
+     * @param $data
+     * @param PurchaseOrder $po
+     * @return PurchaseOrder|null
+     */
+    public function save($data, PurchaseOrder $po): ?PurchaseOrder;
+}
