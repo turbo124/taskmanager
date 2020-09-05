a5a2bb7024426db1fb3f654d112231bb232665a6
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-05 12:18:54 +0100

    Create PurchaseOrderInvitationTransformable.php

---
+++ app/Transformations/PurchaseOrderInvitationTransformable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Transformations;
+
+use App\Models\PurchaseOrderInvitation;
+
+class PurchaseOrderInvitationTransformable
+{
+
+    /**
+     * @param PurchaseOrderInvitation $invitation
+     * @return array
+     */
+    public function transformPurchaseOrderInvitations(PurchaseOrderInvitation $invitation)
+    {
+        return [
+            'id'                => (int)$invitation->id,
+            'company_contact_id' => (int)$invitation->company_contact_id,
+            'company_id'         => (int)$invitation->company_id,
+            'key'               => $invitation->key,
+            'sent_date'         => $invitation->sent_date ?: '',
+            'viewed_date'       => $invitation->viewed_date ?: '',
+            'opened_date'       => $invitation->opened_date ?: '',
+            'updated_at'        => $invitation->updated_at,
+            'archived_at'       => $invitation->deleted_at,
+        ];
+    }
+
+}
