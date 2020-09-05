097d53afd83a8cdd5db6ab2c4eaf590ee6408cf8
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-05 19:01:07 +0100

    Create Contactsm.js

---
+++ resources/assets/js/components/purchase_orders/edit/Contactsm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card, CardBody, CardHeader, FormGroup, Input, Label } from 'reactstrap'
+import { translations } from '../../common/_translations'
+
+export default function Contactsm (props) {
+    const send_to = props.contacts.length ? props.contacts.map((contact, index) => {
+        const invitations = props.invitations.length ? props.invitations.filter(invitation => parseInt(invitation.company_contact_id) === contact.id) : []
+        const checked = invitations.length ? 'checked="checked"' : ''
+        return <FormGroup key={index} check>
+            <Label check>
+                <Input checked={checked} value={contact.id} onChange={props.handleContactChange}
+                    type="checkbox"/> {`${contact.first_name} ${contact.last_name}`}
+            </Label>
+        </FormGroup>
+    }) : null
+    return (
+        <Card>
+            <CardHeader>{translations.invitations}</CardHeader>
+            <CardBody>
+                {send_to}
+
+                {!props.contacts.length &&
+                <h2>You haven't selected a customer</h2>
+                }
+            </CardBody>
+        </Card>
+
+    )
+}
