1f41da7e71b019c52891d4cf7883eb1b8eb54b21
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-06 15:49:21 +0100

    Create PurchaseOrderArchivedActivity.php

---
+++ app/Listeners/PurchaseOrder/PurchaseOrderArchivedActivity.php
@@ -0,0 +1,49 @@
+
+<?php
+
+namespace App\Listeners\PurchaseOrder;
+
+use App\Factory\NotificationFactory;
+use App\Repositories\NotificationRepository;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PurchaseOrderArchivedActivity implements ShouldQueue
+{
+    /**
+     * @var NotificationRepository
+     */
+    protected NotificationRepository $notification_repo;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(NotificationRepository $notification_repo)
+    {
+        $this->notification_repo = $notification_repo;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param object $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $fields = [];
+        $fields['data']['id'] = $event->purchase_order->id;
+        $fields['data']['customer_id'] = $event->purchase_order->customer_id;
+        $fields['data']['message'] = 'A purchase order was archived';
+        $fields['notifiable_id'] = $event->purchase_order->user_id;
+        $fields['account_id'] = $event->purchase_order->account_id;
+        $fields['notifiable_type'] = get_class($event->purchase_order);
+        $fields['type'] = get_class($this);
+        $fields['data'] = json_encode($fields['data']);
+
+        $notification = NotificationFactory::create($event->purchase_order->account_id, $event->purchase_order->user_id);
+        $notification->entity_id = $event->purchase_order->id;
+        $this->notification_repo->save($notification, $fields);
+    }
+}
