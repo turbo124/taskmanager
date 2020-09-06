3f2e75443636672c42bce16df9f40803ea225bf8
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-06 17:17:05 +0100

    Create CaseWasArchived.php

---
+++ app/Events/Cases/Cases/CaseWasArchived.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events\Cases;
+
+use App\Models\Cases;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Class InvoiceWasMarkedSent.
+ */
+class CaseWasArchived
+{
+    use SerializesModels;
+
+    /**
+     * @var case
+     */
+    public Cases $case;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param case $case
+     */
+    public function __construct(Cases $case)
+    {
+        $this->case = $case;
+    }
+}
