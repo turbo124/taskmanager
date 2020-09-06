2374f4fa0171ea6bb46807d22cdc70cfe54151ea
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-06 17:08:51 +0100

    Create TaskWasUpdated.php

---
+++ app/Events/Task/TaskWasUpdated.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events\Task;
+
+use App\Models\task;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Class InvoiceWasMarkedSent.
+ */
+class TaskWasUpdated
+{
+    use SerializesModels;
+
+    /**
+     * @var task
+     */
+    public Task $task;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Task $task
+     */
+    public function __construct(task $task)
+    {
+        $this->task = $task;
+    }
+}
