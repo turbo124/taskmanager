24a89a6b54a884ac9f67769d323c5dd648682830
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-08 18:42:39 +0100

    Update StoreGroupRequest.php

--- app/Requests/Group/StoreGroupRequest.php
+++ app/Requests/Group/StoreGroupRequest.php
@@ -10,7 +10,7 @@ class StoreGroupRequest extends BaseFormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
+             'name' => ['required', 'unique:groups']
         ];
     }
 }
