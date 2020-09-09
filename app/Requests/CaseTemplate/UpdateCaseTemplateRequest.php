28d1310b7f66d8fe281e74e3d55cac583db21952
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-09 20:30:42 +0100

    Create UpdateCaseTemplateRequest.php

---
+++ app/Requests/CaseTemplate/UpdateCaseTemplateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Requests\CaseTemplate;
+
+
+use App\Repositories\Base\BaseFormRequest;
+
+class UpdateCaseTemplateRequest extends BaseFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'unique:case_templates'],
+            'description' => 'required',
+            'send_on' => 'required'
+        ];
+    }
+}
