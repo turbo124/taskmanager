7f726cd9e07251cc869e5c288a39a05b445d5144
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-12 19:47:41 +0100

    Create CalculateRecurringDateRanges.php

---
+++ app/Traits/CalculateRecurringDateRanges.php
@@ -0,0 +1,17 @@
+<?php
+
+trait CalculateRecurringDateRanges
+{
+
+    public function calculate($start_date, $end_date) {
+        $begin = new DateTime($start_date);
+        $end = new DateTime($end_date);
+
+        $interval = DateInterval::createFromDateString('1 day');
+        $period = new DatePeriod($begin, $interval, $end);
+
+        foreach ($period as $dt) {
+            echo $dt->format("l Y-m-d H:i:s\n");
+        }
+    }
+}
