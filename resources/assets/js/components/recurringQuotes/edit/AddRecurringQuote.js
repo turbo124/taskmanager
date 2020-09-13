41deea19dd510aeefdc0fe6f2a6dfcabc83f8af9
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-13 14:16:22 +0100

    Update AddRecurringQuote.js

--- resources/assets/js/components/recurringQuotes/edit/AddRecurringQuote.js
+++ resources/assets/js/components/recurringQuotes/edit/AddRecurringQuote.js
@@ -58,7 +58,9 @@ class AddRecurringQuote extends Component {
             custom_value3: this.state.custom_value3,
             custom_value4: this.state.custom_value4,
             public_notes: this.state.public_notes,
-            private_notes: this.state.private_notes
+            private_notes: this.state.private_notes,
+            grace_period: this.state.grace_period,
+            auto_billing_enabled: this.state.auto_billing_enabled
         }
 
         this.recurringQuoteModel.save(data).then(response => {
