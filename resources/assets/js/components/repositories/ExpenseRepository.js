3a7f628dc08b96512d6932f2605b5430db4a437a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-27 09:39:14 +0100

    Create ExpenseRepository.js

---
+++ resources/assets/js/components/repositories/ExpenseRepository.js
@@ -0,0 +1,22 @@
+export default class ExpenseRepository {
+
+async get () {
+        this.errors = []
+        this.error_message = ''
+
+        try {
+            const res = await axios.get(this._url)
+
+            if (res.status === 200) {
+                // test for status you want, etc
+                console.log(res.status)
+            }
+
+            // Don't forget to return something
+            return res.data
+        } catch (e) {
+            this.handleError(e)
+            return false
+        }
+    }
+}
