46620fa4ff5b808181dd12ec2363293f6cf9ecd0
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-27 21:07:05 +0100

    Update BuildVariables.php

--- app/Traits/BuildVariables.php
+++ app/Traits/BuildVariables.php
@@ -12,6 +12,14 @@ trait BuildVariables
      */
     public function parseVariables($content)
     {
+        $this->objPdf->build();
 
+        $labels = $this->objPdf->getLabels();
+        $values = $this->objPdf->getValues();
+
+        $content = $this->objPdf->parseLabels($labels, $content);
+        $content = $this->objPdf->parseValues($values, $content);
+
+        return $content;
     }
 }
