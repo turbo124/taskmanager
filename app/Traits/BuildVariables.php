623a4cc876650a770db4e608ced2a84548d6aa65
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-27 21:12:40 +0100

    Update BuildVariables.php

--- app/Traits/BuildVariables.php
+++ app/Traits/BuildVariables.php
@@ -6,6 +6,17 @@ namespace App\Traits;
 
 trait BuildVariables
 {
+    public function formatNotes($entity) {
+        if(isset($entity->public_notes) && strlen($entity->public_notes) > 0) {
+            $entity->public_notes = $this->parseVariables($entity->public_notes);
+        }
+
+        if(isset($entity->private_notes) && strlen($entity->private_notes) > 0) {
+            $entity->private_notes = $this->parseVariables($entity->private_notes);
+        }
+
+        return $entity;
+    }
 
     /**
      * @param $amount
