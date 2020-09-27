aafe37a93d5c0dea3e3e11f98e4daae748e861ca
michael-hampton <michaelhamptondesign@yahoo.com>
2020-09-27 15:16:48 +0100

    Update CreditModel.js

--- resources/assets/js/components/models/CreditModel.js
+++ resources/assets/js/components/models/CreditModel.js
@@ -109,6 +109,10 @@ export default class CreditModel extends BaseModel {
         return this.fields.exchange_rate
     }
 
+    get isNew () {
+        return this.fields.id && this.fields.id.toString().length && parseInt(this.fields.id) > 0
+    }
+
     set exchange_rate (exchange_rate) {
         this.fields.exchange_rate = exchange_rate
     }
