6642285c63e0844872f66e74117d1bbd862521f2
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-04 20:22:06 +0100

    Create ReverseInvoicePayment.php

---
+++ app/Components/Payment/Invoice/ReverseInvoicePayment.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Components\Payment\Invoice;
+
+use App\Models\Customer;
+use App\Models\Invoice;
+use App\Models\Payment;
+
+/**
+ * Class ReverseInvoicePayment
+ * @package App\Services\Payment
+ */
+class ReverseInvoicePayment
+{
+    private Payment $payment;
+
+    public function __construct(Payment $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    /**
+     * @return Payment|null
+     */
+    public function execute(): ?Payment
+    {
+        $this->reversePayment();
+        $this->updateCustomer();
+        $this->payment->transaction_service()->createTransaction(
+            $this->payment->amount,
+            $this->payment->customer->balance
+        );
+        $this->payment->deletePayment();
+
+        return $this->payment;
+    }
+
+    /**
+     * @return bool
+     */
+    private function reversePayment(): bool
+    {
+        $invoices = $this->payment->invoices;
+
+        if(empty($invoices)) {
+            return true;
+        }
+
+        $delete_status = !empty() ? (int) $this->payment->customer->getSetting('invoice_payment_deleted_status') : Invoice::STATUS_SENT;
+
+        foreach ($invoices as $invoice) {
+            if ($invoice->pivot->amount <= 0) {
+                continue;
+            }
+
+            if($delete_status === 100) {
+                $invoice->delete();
+                continue;
+            }
+
+            $invoice->setStatus($delete_status);
+            $invoice->setBalance($invoice->pivot->amount);
+            $invoice->save();
+        }
+
+        return true;
+    }
+
+    /**
+     * @return Customer
+     */
+    private function updateCustomer(): Customer
+    {
+        $customer = $this->payment->customer;
+        $customer->increaseBalance($this->payment->amount);
+        $customer->reducePaidToDateAmount($this->payment->amount);
+        $customer->save();
+        return $customer;
+    }
+}
