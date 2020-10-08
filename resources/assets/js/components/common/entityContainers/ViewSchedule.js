ac68e45efef115204ba68f3941e8ec887006fef9
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-08 17:51:49 +0100

    Update ViewSchedule.js

--- resources/assets/js/components/common/entityContainers/ViewSchedule.js
+++ resources/assets/js/components/common/entityContainers/ViewSchedule.js
@@ -7,38 +7,21 @@ import FormatDate from '../FormatDate'
 
 export default function ViewSchedule (props) {
     const listClass = !Object.prototype.hasOwnProperty.call(localStorage, 'dark_theme') || (localStorage.getItem('dark_theme') && localStorage.getItem('dark_theme') === 'true') ? 'list-group-item-dark' : ''
-    const customer = props.customers && props.customers.length ? props.customers.filter(customer => customer.id === parseInt(props.entity.customer_id)) : []
+   
+        contactList = this.props.recurringDates.map((schedule, index) => {
 
-    let contactList = null
-
-    if (customer.length) {
-        const customerModel = new CustomerModel(customer[0])
-        const invitations = props.entity.invitations
-
-        contactList = invitations.map((invitation, index) => {
-            const link = props.entity.invitation_link.replace('$key', invitation.key)
-            const contact = customerModel.findContact(invitation.contact_id)
-
-            return <ListGroupItem key={index} className={listClass}>
-                <a href={link}>
-                    <ListGroupItemHeading><i
-                        className={`fa ${icons.contact} mr-4`}/>{(!contact.fullName.length ? customerModel.displayName : contact.fullName)}
-                    </ListGroupItemHeading>
-                    {!!invitation.sent_date.length &&
-                    <p>{translations.sent} <FormatDate date={invitation.sent_date} with_time={true}/></p>
-                    }
-                    {!!invitation.viewed_date.length &&
-                    <p>{translations.viewed} <FormatDate date={invitation.viewed_date} with_time={true}/></p>
-                    }
-                </a>
-                <p className="small"><span
-                    onClick={(e) => props.entity.copyToClipboard(link)}> {translations.copy_to_clipboard}</span>
-                </p>
-            </ListGroupItem>
+            return  <ListGroupItem className="d-flex justify-content-between align-items-center"><FormatDate date={schedule.send_date} /> <FormatDate date={schedule.due_date} /> </ListGroupItem>
         })
-    }
 
-    return <ListGroup className="mt-2 col-12">
-        {contactList}
-    </ListGroup>
+    return <React.Fragment>
+        <ListGroup horizontal>
+        <ListGroupItem tag="a" href="#">{translations.send_date}</ListGroupItem>
+        <ListGroupItem tag="a" href="#">{translations.due_date} </ListGroupItem>
+      </ListGroup>
+
+        <ListGroup className="mt-2 col-12">
+            {contactList}
+        </ListGroup>
+    </React.Fragment>
+    
 }
