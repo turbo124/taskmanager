68f0102fe051099ce31316eb3684b53da0e28004
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-15 22:10:06 +0100

    Update RecurringInvoiceToInvoiceFactory.php

--- app/Factory/RecurringInvoiceToInvoiceFactory.php
+++ app/Factory/RecurringInvoiceToInvoiceFactory.php
@@ -20,6 +20,7 @@ class RecurringInvoiceToInvoiceFactory
     public static function create(RecurringInvoice $recurring_invoice, Customer $customer): Invoice
     {
         $invoice = new Invoice();
+        $invoice->fill($recurring_invoice->toArray());
         $invoice->setAccount($recurring_invoice->account);
         $invoice->setStatus(Invoice::STATUS_DRAFT);
         $invoice->setCustomer($recurring_invoice->customer);
@@ -27,28 +28,7 @@ class RecurringInvoiceToInvoiceFactory
         $invoice->setTotal($recurring_invoice->total);
         $invoice->setBalance($recurring_invoice->total);
         $invoice->setUser($recurring_invoice->user);
-
-        $invoice->sub_total = $recurring_invoice->sub_total;
-        $invoice->tax_total = $recurring_invoice->tax_total;
-        $invoice->discount_total = $recurring_invoice->discount_total;
-        $invoice->tax_rate = $recurring_invoice->tax_rate;
-        $invoice->is_amount_discount = $recurring_invoice->is_amount_discount;
-        $invoice->po_number = $recurring_invoice->po_number;
-        $invoice->footer = $recurring_invoice->footer;
-        $invoice->terms = $recurring_invoice->terms;
-        $invoice->public_notes = $recurring_invoice->public_notes;
-        $invoice->private_notes = $recurring_invoice->private_notes;
-        $invoice->date = date_create()->format('Y-m-d');
-        $invoice->is_deleted = false;
-        $invoice->line_items = $recurring_invoice->line_items;
         $invoice->recurring_invoice_id = $recurring_invoice->id;
-        $invoice->transaction_fee = $recurring_invoice->transaction_fee;
-        $invoice->shipping_cost = $recurring_invoice->shipping_cost;
-        $invoice->transaction_fee_tax = $recurring_invoice->transaction_fee_tax;
-        $invoice->shipping_cost_tax = $recurring_invoice->shipping_cost_tax;
-        $invoice->gateway_fee = $recurring_invoice->gateway_fee ?: 0;
-        $invoice->gateway_percentage = $recurring_invoice->gateway_percentage;
-        $invoice->auto_billing_enabled = $recurring_invoice->auto_billing_enabled;
 
         return $invoice;
     }
