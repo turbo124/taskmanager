41744357b27f3d67b619f43f0f11a4720582eae0
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-15 22:01:32 +0100

    Update CloneQuoteToOrderFactory.php

--- app/Factory/CloneQuoteToOrderFactory.php
+++ app/Factory/CloneQuoteToOrderFactory.php
@@ -21,7 +21,8 @@ class CloneQuoteToOrderFactory
      */
     public static function create(Quote $quote, User $user, Account $account): ?Order
     {
-        $order = new Order;
+        $order = new Order; 
+        $order->fill($quote->toArray()));
         $order->setAccount($account);
         $order->setCustomer($quote->customer);
         $order->setUser($user);
@@ -30,22 +31,8 @@ class CloneQuoteToOrderFactory
         $order->setNumber();
         $order->setDueDate();
         $order->setBalance($quote->total);
-
         $order->quote_id = $quote->id;
-        $order->task_id = $quote->task_id;
-        $order->discount_total = $quote->discount_total;
-        $order->tax_total = $quote->tax_total;
-        $order->is_amount_discount = $quote->is_amount_discount ?: false;
-        $order->footer = $quote->footer;
-        $order->public_notes = $quote->public_notes;
-        $order->private_notes = $quote->private_notes;
-        $order->tax_rate = $quote->tax_rate;
-        $order->terms = $quote->terms;
-        $order->sub_total = $quote->sub_total;
-        $order->partial = $quote->partial;
-        $order->partial_due_date = $quote->partial_due_date;
-        $order->date = $quote->date;
-        $order->line_items = $quote->line_items;
+       
         return $order;
     }
 }
