c7d46742001200a7884ee1c32915acdffe54d9b9
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-15 22:07:53 +0100

    Update QuoteToRecurringQuoteFactory.php

--- app/Factory/QuoteToRecurringQuoteFactory.php
+++ app/Factory/QuoteToRecurringQuoteFactory.php
@@ -18,39 +18,16 @@ class QuoteToRecurringQuoteFactory
     public static function create(Quote $quote): RecurringQuote
     {
         $recurring_invoice = new RecurringQuote;
+        $recurring_invoice->fill($quote->toArray());
         $recurring_invoice->status_id = RecurringQuote::STATUS_DRAFT;
-        $recurring_invoice->discount_total = $quote->discount_total;
-        $recurring_invoice->tax_total = $quote->tax_total;
         $recurring_invoice->number = '';
-        $recurring_invoice->is_amount_discount = $quote->is_amount_discount;
-        $recurring_invoice->po_number = $quote->po_number;
-        $recurring_invoice->footer = $quote->footer;
-        $recurring_invoice->terms = $quote->terms;
-        $recurring_invoice->public_notes = $quote->public_notes;
-        $recurring_invoice->private_notes = $quote->private_notes;
-        $recurring_invoice->tax_rate_name = $quote->tax_rate_name;
-        $recurring_invoice->tax_rate = $quote->tax_rate;
         $recurring_invoice->date = date_create()->format('Y-m-d');
         $recurring_invoice->customer_id = $quote->customer_id;
-        $recurring_invoice->due_date = $recurring_invoice->setDueDate();
-        $recurring_invoice->is_deleted = $quote->is_deleted;
-        $recurring_invoice->line_items = $quote->line_items;
-        $recurring_invoice->custom_value1 = $quote->custom_value1;
-        $recurring_invoice->custom_value2 = $quote->custom_value2;
-        $recurring_invoice->custom_value3 = $quote->custom_value3;
-        $recurring_invoice->custom_value4 = $quote->custom_value4;
-        $recurring_invoice->total = $quote->total;
-        $recurring_invoice->sub_total = $quote->sub_total;
-        $recurring_invoice->balance = $quote->balance;
+        $recurring_invoice->balance = $quote->total;
         $recurring_invoice->user_id = $quote->user_id;
         $recurring_invoice->account_id = $quote->account_id;
-        $recurring_invoice->transaction_fee = $quote->transaction_fee;
-        $recurring_invoice->shipping_cost = $quote->shipping_cost;
-        $recurring_invoice->transaction_fee_tax = $quote->transaction_fee_tax;
-        $recurring_invoice->shipping_cost_tax = $quote->shipping_cost_tax;
-        $recurring_invoice->gateway_fee = $quote->gateway_fee;
-        $recurring_invoice->gateway_percentage = $quote->gateway_percentage;
         $recurring_invoice->frequency = 30;
+        
         return $recurring_invoice;
     }
 }
