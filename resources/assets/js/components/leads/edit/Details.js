583be68d175056278551bebd7b1dca4e944ba2fd
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-17 15:37:27 +0100

    Update Details.js

--- resources/assets/js/components/leads/edit/Details.js
+++ resources/assets/js/components/leads/edit/Details.js
@@ -3,6 +3,7 @@ import { Card, CardBody, CardHeader, FormGroup, Input, Label } from 'reactstrap'
 import UserDropdown from '../../common/dropdowns/UserDropdown'
 import { translations } from '../../utils/_translations'
 import TaskStatusDropdown from '../../common/dropdowns/TaskStatusDropdown'
+import DesignDropdown from './dropdowns/DesignDropdown'
 
 export default class Details extends React.Component {
     constructor (props) {
@@ -101,6 +102,11 @@ export default class Details extends React.Component {
                                 status={this.props.lead.task_status}
                                 handleInputChanges={this.props.handleInputChanges}
                             />
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Label>{translations.design}</Label>
+                            <DesignDropdown name="design_id" design={props.design_id} handleChange={props.handleInput}/>
                         </FormGroup>
                     </CardBody>
                 </Card>
