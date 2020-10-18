cd13a81f6b473af74a4081b67a2598fad009e81d
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-18 20:14:16 +0100

    Update TaskList.js

--- resources/assets/js/components/tasks/TaskList.js
+++ resources/assets/js/components/tasks/TaskList.js
@@ -47,6 +47,7 @@ export default class TaskList extends Component {
             custom_fields: [],
 
             ignoredColumns: [
+                'invoice',
                 'project',
                 'files',
                 'emails',
