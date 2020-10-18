f3fad73d06cf7b0e4b97a6a4bba24cb2b887dc09
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-18 17:18:02 +0100

    Update RecurringInvoices.js

--- resources/assets/js/components/recurringInvoices/RecurringInvoices.js
+++ resources/assets/js/components/recurringInvoices/RecurringInvoices.js
@@ -40,6 +40,8 @@ export default class RecurringInvoices extends Component {
                 end_date: ''
             },
             showRestoreButton: false,
+            entity_id: queryString.parse(this.props.location.search).entity_id || false,
+            entity_type: queryString.parse(this.props.location.search).entity_type || false,
             custom_fields: [],
             ignoredColumns: ['schedule', 'grace_period', 'last_sent_date', 'invoices', 'currency_id', 'exchange_rate', 'gateway_fee', 'transaction_fee', 'shipping_cost', 'gateway_percentage', 'transaction_fee_tax', 'shipping_cost_tax', 'audits', 'invitations', 'files', 'id', 'custom_value1', 'invoice_id', 'custom_value2', 'custom_value3', 'custom_value4', 'updated_at', 'deleted_at', 'created_at', 'public_notes', 'private_notes', 'use_inclusive_taxes', 'terms', 'footer', 'line_items', 'date_to_send', 'first_name', 'last_name', 'tax_total', 'discount_total', 'sub_total']
 
