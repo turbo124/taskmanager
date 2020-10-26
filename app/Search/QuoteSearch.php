7622cca6997307f32b1a82eae1c9198b0e437a1b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-26 18:39:06 +0000

    Users

--- app/Search/QuoteSearch.php
+++ app/Search/QuoteSearch.php
@@ -50,6 +50,10 @@ class QuoteSearch extends BaseSearch
             $this->query = $this->searchFilter($request->search_term);
         }
 
+        if ($request->filled('user_id')) {
+            $this->query->where('assigned_to', '=', $request->user_id);
+        }
+
         if ($request->input('start_date') <> '' && $request->input('end_date') <> '') {
             $this->filterDates($request);
         }
