a85e445e4e8af4f5add6f7203d1357169581c413
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-30 21:14:52 +0000

    Recurring Invoices

--- resources/assets/js/components/utils/_consts.js
+++ resources/assets/js/components/utils/_consts.js
@@ -337,19 +337,11 @@ export const casePriorities = {
 }
 
 export const frequencyOptions = {
-    1: 'frequency_daily',
-    7: 'frequency_weekly',
-    14: 'frequency_two_weeks',
-    28: 'frequency_four_weeks',
-    31: 'frequency_monthly',
-    60: 'frequency_two_months',
-    91: 'frequency_three_months',
-    121: 'frequency_four_months',
-    182: 'frequency_six_months',
-    365: 'frequency_annually',
-    730: 'frequency_two_years',
-    1095: 'frequency_three_years',
-    9000: 'frequency_endless'
+    'DAILY': 'frequency_daily',
+     'WEEKLY': 'frequency_weekly',
+     'MONTHLY': 'frequency_monthly',
+     'YEARLY': 'frequency_annually',
+    'ENDLESS': 'frequency_endless'
 }
 
 export const taskTypes = {
