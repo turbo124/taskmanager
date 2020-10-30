175442310e91bdb6dcb206f41dad3e9bb6437202
michael-hampton <michaelhamptondesign@yahoo.com>
2020-10-30 21:24:17 +0000

    Create CalculateRecurring.php

---
+++ app/Traits/CalculateRecurring.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Traits;
+
+
+use Carbon\Carbon;
+
+trait CalculateRecurring
+{
+
+    public function calculate($frequency) {
+        switch($frequency) {
+            case 'DAILY':
+
+            break;
+
+            case 'WEEKLY':
+
+            break;
+
+            case 'MONTHLY':
+
+            break;
+
+            case 'YEARLY':
+
+            break;
+        }
+    }
+
+}
