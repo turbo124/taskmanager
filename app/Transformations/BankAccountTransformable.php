1afaa3db17d798da4523243cf2814491d5b5052c
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-07 17:01:52 +0000

    Create BankAccountTransformable.php

---
+++ app/Transformations/BankAccountTransformable.php
@@ -0,0 +1,25 @@
+
+<?php
+
+namespace App\Transformations;
+
+use App\Models\BankAccount;
+
+trait BankAccountTransformable
+{
+
+    /**
+     * @param BankAccount $bank_account
+     * @return array
+     */
+    protected function transformBankAccount(BankAccount $bank_account)
+    {
+        return [
+            'id'          => (int)$bank_account->id,
+            'name'        => $bank_account->name,
+            'username'    => $bank_account->username,
+            'password'    => $bank_account->password
+        ];
+    }
+
+}
