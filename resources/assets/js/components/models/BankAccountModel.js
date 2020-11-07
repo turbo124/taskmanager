c85cafa147e522c924f8ed9293b0479ef286c84d
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-07 14:44:15 +0000

    Create BankAccountModel.js

---
+++ resources/assets/js/components/models/BankAccountModel.js
@@ -0,0 +1,141 @@
+import axios from 'axios'
+import BaseModel from './BaseModel'
+
+export default class BankAccountModel extends BaseModel {
+    constructor (data = null) {
+        super()
+
+        this._url = '/api/bank_accounts'
+        this.entity = 'BankAccount'
+
+        this._fields = {
+            number: '',
+            id: null,
+            modal: false,
+            name: '',
+            description: '',
+            private_notes: '',
+            public_notes: '',
+            custom_value1: '',
+            custom_value2: '',
+            custom_value3: '',
+            custom_value4: '',
+            assigned_to: '',
+            username: '',
+            password: '',
+            errors: []
+        }
+
+        if (data !== null) {
+            this._fields = { ...this.fields, ...data }
+        }
+
+        this._file_count = 0
+
+        if (data !== null && data.files) {
+            this.fileCount = data.files
+        }
+    }
+
+    get id () {
+        return this.fields.id
+    }
+
+    get fileCount () {
+        return this._file_count || 0
+    }
+
+    set fileCount (files) {
+        this._file_count = files ? files.length : 0
+    }
+
+    get fields () {
+        return this._fields
+    }
+
+    get url () {
+        return this._url
+    }
+
+    buildDropdownMenu () {
+        const actions = []
+
+        if (!this.fields.is_deleted) {
+            actions.push('delete')
+        }
+
+        if (!this.fields.deleted_at) {
+            actions.push('archive')
+        }
+
+        return actions
+    }
+
+    async update (data) {
+        if (!this.fields.id) {
+            return false
+        }
+
+        this.errors = []
+        this.error_message = ''
+
+        try {
+            const res = await axios.put(`${this.url}/${this.fields.id}`, data)
+
+            if (res.status === 200) {
+                // test for status you want, etc
+                console.log(res.status)
+            }
+            // Don't forget to return something
+            return res.data
+        } catch (e) {
+            this.handleError(e)
+            return false
+        }
+    }
+
+    async completeAction (data, action) {
+        if (!this.fields.id) {
+            return false
+        }
+
+        this.errors = []
+        this.error_message = ''
+
+        try {
+            const res = await axios.post(`${this.url}/${this.fields.id}/${action}`, data)
+
+            if (res.status === 200) {
+                // test for status you want, etc
+                console.log(res.status)
+            }
+            // Don't forget to return something
+            return res.data
+        } catch (e) {
+            this.handleError(e)
+            return false
+        }
+    }
+
+    async save (data) {
+        if (this.fields.id) {
+            return this.update(data)
+        }
+
+        try {
+            this.errors = []
+            this.error_message = ''
+            const res = await axios.post(this.url, data)
+
+            if (res.status === 200) {
+                // test for status you want, etc
+                console.log(res.status)
+            }
+            // Don't forget to return something
+            return res.data
+        } catch (e) {
+            this.handleError(e)
+            return false
+        }
+    }
+}
