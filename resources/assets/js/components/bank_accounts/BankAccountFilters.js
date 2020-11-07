ec40e6bc69a4c68f1b9e4f03e05139507f09b8c4
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-07 14:02:37 +0000

    Create BankAccountFilters.js

---
+++ resources/assets/js/components/bank_accounts/BankAccountFilters.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+import { Col, FormGroup, Row } from 'reactstrap'
+import CustomerDropdown from '../common/dropdowns/CustomerDropdown'
+import TableSearch from '../common/TableSearch'
+import DateFilter from '../common/DateFilter'
+import CsvImporter from '../common/CsvImporter'
+import FilterTile from '../common/FilterTile'
+import StatusDropdown from '../common/StatusDropdown'
+
+export default class BankAccountFilters extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            isOpen: false,
+            dropdownButtonActions: ['download'],
+            filters: {
+                status_id: 'active',
+                //customer_id: '',
+                user_id: '',
+                searchText: '',
+                start_date: '',
+                end_date: ''
+            }
+
+        }
+
+        this.filterBankAccounts = this.filterBankAccounts.bind(this)
+        this.getFilters = this.getFilters.bind(this)
+    }
+
+    setFilterOpen (isOpen) {
+        this.setState({ isOpen: isOpen })
+    }
+
+    filterBankAccounts (event) {
+        if ('start_date' in event) {
+            this.setState(prevState => ({
+                filters: {
+                    ...prevState.filters,
+                    start_date: event.start_date,
+                    end_date: event.end_date
+                }
+            }), () => this.props.filter(this.state.filters))
+            return
+        }
+
+        const column = event.target.id
+        const value = event.target.value
+
+        if (value === 'all') {
+            const updatedRowState = this.state.filters.filter(filter => filter.column !== column)
+            this.setState({ filters: updatedRowState }, () => this.props.filter(this.state.filters))
+            return true
+        }
+
+        this.setState(prevState => ({
+            filters: {
+                ...prevState.filters,
+                [column]: value
+            }
+        }), () => this.props.filter(this.state.filters))
+
+        return true
+    }
+
+    getFilters () {
+        const { status_id, searchText, start_date, end_date } = this.props.filters
+
+        return (
+            <Row form>
+                <Col md={3}>
+                    <TableSearch onChange={this.filterBankAccounts}/>
+                </Col>
+
+                <Col sm={12} md={3} className="mt-3 mt-md-0">
+                    <CustomerDropdown
+                        customer={this.props.filters.customer_id}
+                        handleInputChanges={this.filterProjects}
+                    />
+                </Col>
+
+                <Col sm={12} md={2} className="mt-3 mt-md-0">
+                    <FormGroup>
+                        <StatusDropdown filterStatus={this.filterProjects}/>
+                    </FormGroup>
+                </Col>
+
+                <Col sm={12} md={1} className="mt-3 mt-md-0">
+                    <CsvImporter filename="project.csv"
+                        url={`/api/bank_accounts?search_term=${searchText}&status=${status_id}&start_date=${start_date}&end_date=${end_date}&page=1&per_page=5000`}/>
+                </Col>
+
+                <Col sm={12} md={3} className="mt-3 mt-md-0">
+                    <FormGroup>
+                        <DateFilter onChange={this.filterProjects}/>
+                    </FormGroup>
+                </Col>
+            </Row>
+        )
+    }
+
+    render () {
+        const filters = this.getFilters()
+
+        return (<FilterTile setFilterOpen={this.props.setFilterOpen} filters={filters}/>)
+    }
+}
