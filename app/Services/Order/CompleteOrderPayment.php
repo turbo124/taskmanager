4c1ed834654a4369463e6632e38284aa0c7f366a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-14 13:10:48 +0000

    Update CompleteOrderPayment.php

--- app/Services/Order/CompleteOrderPayment.php
+++ app/Services/Order/CompleteOrderPayment.php
@@ -1,15 +1,47 @@
 <?php
+
+namespace App\Services\Order;
+
+use App\Models\Order;
+use App\Models\Invoice;
+use App\Models\Payment;
+use App\Models\Product;
+use App\Models\ProductAttribute;
+
 class CompleteOrderPayment
 {
-    private function execute()
-    {
-       
-        
-       
+    /**
+     * @var Order
+     */
+    private Order $order;
 
-       
+    /**
+     * CompleteOrderPayment constructor.
+     * @param Order $order
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
     }
 
+    private function execute(): bool
+    {
+       if(!$this->capturePayment()) {
+           return false;
+       }
+
+       $this->updateInvoice();
+       $this->updatePayment();
+       $this->updateCustomer();
+       $this->updateOrder();
+
+       return true;
+    }
+   
+    private function capturePayment()
+    {
+        //TODO
+    }
 
     private function updateOrder (): bool
     {
