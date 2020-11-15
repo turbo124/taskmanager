2ecdfbf44c4e1cd9b7ba1225d6549652bee90bc2
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-15 14:37:11 +0000

    Create ExpenseRepositoryInterface.php

---
+++ app/Repositories/Interfaces/ExpenseRepositoryInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\Models\Account;
+use App\Models\Expense;
+use App\Models\Task;
+use App\Requests\SearchRequest;
+use Illuminate\Support\Collection;
+
+interface ExpenseRepositoryInterface
+{
+    /**
+     *
+     * @param string $order
+     * @param string $sort
+     * @param array $columns
+     */
+    public function getAll(SearchRequest $search_request, Account $account);
+
+    /**
+     * @param int $id
+     * @return Expense
+     */
+    public function findExpenseById(int $id): Expense;
+
+    /**
+     * @param array $data
+     * @param Expense $expense
+     * @return Expense|null
+     */
+    public function save(array $data, Expense $expense): ?Expense;
+
+    /**
+     * @param array $data
+     * @param Expense $expense
+     * @return Expense|null
+     */
+    public function createExpense(array $data, Expense $expense): ?Expense;
+
+    /**
+     * @param array $data
+     * @param Expense $expense
+     * @return Expense|null
+     */
+    public function updateExpense(array $data, Expense $expense): ?Expense;
+
+}
