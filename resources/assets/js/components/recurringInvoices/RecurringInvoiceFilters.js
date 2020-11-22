6f5637ddcbcc8a9752ed423a71e153aa3f42888e
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-22 17:05:37 +0000

    Update RecurringInvoiceFilters.js

--- resources/assets/js/components/recurringInvoices/RecurringInvoiceFilters.js
+++ resources/assets/js/components/recurringInvoices/RecurringInvoiceFilters.js
@@ -90,7 +90,7 @@ export default class RecurringInvoiceFilters extends Component {
                 </Col>
 
                 <Col sm={12} md={1} className="mt-3 mt-md-0">
-                    <CsvImporter filename="recurringInvoices.csv"
+                    <CsvImporter customers={this.props.customers} filename="recurringInvoices.csv"
                         url={`/api/recurring-invoice?search_term=${searchText}&status=${status_id}&customer_id=${customer_id}&project_id=${project_id}&user_id=${user_id}&start_date=${start_date}&end_date=${end_date}&page=1&per_page=5000`}/>
                 </Col>
 
