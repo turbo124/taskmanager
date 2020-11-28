db68f74f032a3087a9fb4802a77e74b0399b05e5
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-28 12:29:00 +0000

    Update TaxRate.php

--- app/Models/TaxRate.php
+++ app/Models/TaxRate.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Archiveable;
 
 class TaxRate extends Model
 {
 
     use SoftDeletes;
     use HasFactory;
+    use Archiveable;
 
     /**
      * The attributes that are mass assignable.
