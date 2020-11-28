86d7af779f6914a1564864218e44cf0f0230d378
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-28 12:22:29 +0000

    Update ProductFeature.php

--- app/Models/ProductFeature.php
+++ app/Models/ProductFeature.php
@@ -4,6 +4,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Archiveable;
 
 /**
  * Class ProductFeature
@@ -11,6 +12,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class ProductFeature extends Model
 {
+    use Archiveable;
+
     protected $fillable = [
         'description',
         'product_id'
