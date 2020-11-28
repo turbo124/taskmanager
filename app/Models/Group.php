aa0bc84f9706747c7b0f9701187ba01920c161d4
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-28 12:17:45 +0000

    Update Group.php

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Archiveable;
 
 class Group extends Model
 {
     use SoftDeletes;
     use HasFactory;
+    use Archiveable;
 
     public $timestamps = false;
     protected $casts = [
