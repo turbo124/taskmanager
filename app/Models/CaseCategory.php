deb826c950468217cd5ff1e980429d5d370a025b
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-28 12:09:04 +0000

    Update CaseCategory.php

--- app/Models/CaseCategory.php
+++ app/Models/CaseCategory.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\Archiveable;
 
 class CaseCategory extends Model
 {
 
     use SoftDeletes;
+    use Archiveable;
 
     protected $fillable = [
         'name',
