b7fc7ddd8b3636bb593dc449e1067d16ed1bf0df
michael-hampton <michaelhamptondesign@yahoo.com>
2020-11-29 12:50:40 +0000

    Update EditInvoice.js

--- resources/assets/js/components/invoice/edit/EditInvoice.js
+++ resources/assets/js/components/invoice/edit/EditInvoice.js
@@ -527,7 +527,7 @@ class EditInvoice extends Component {
     reload (data) {
         this.invoiceModel = new InvoiceModel(data, this.props.customers)
         this.initialState = this.invoiceModel.fields
-        this.state = this.initialState
+        this.setState(this.initialState)
     }
 
     buildForm () {
