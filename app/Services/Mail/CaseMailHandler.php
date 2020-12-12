637e3130f3e6a8c424bd72e8e27162c79a5f380a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-12 16:12:54 +0000

    Update CaseMailHandler.php

--- app/Services/Mail/CaseMailHandler.php
+++ app/Services/Mail/CaseMailHandler.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App\Services\Mail;
+
+use App\Models\Cases;
+use BeyondCode\Mailbox\InboundEmail;
+
+class CaseMailHandler {
+    public function __invoke(InboundEmail $email) {
+        Cases::create([
+            'sender'    => $email->from(),
+            'subject'   => $email->subject(),
+            'body'      => $email->text(),
+        ]);
+    }
+}
