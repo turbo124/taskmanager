8c093117e66744ac12356ba98885523c6b88eb36
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-12 16:14:05 +0000

    Update LeadMailHandler.php

--- app/Services/Mail/LeadMailHandler.php
+++ app/Services/Mail/LeadMailHandler.php
@@ -1,1 +1,16 @@
+hp
 
+namespace App;
+
+use App\Models\Lead;
+use BeyondCode\Mailbox\InboundEmail;
+
+class LeadMailHandler {
+    public function __invoke(InboundEmail $email) {
+        ReceivedMail::create([
+            'first_name'    => $email->from(),
+            'name'          => $email->subject(),
+            'description'   => $email->text(),
+        ]);
+    }
+}
