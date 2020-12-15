bd007f560a508f73574767c9affca8241730ad25
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:19:45 +0000

    Update LeadPolicy.php

--- app/Policies/LeadPolicy.php
+++ app/Policies/LeadPolicy.php
@@ -30,6 +30,7 @@ class LeadPolicy extends BasePolicy
      */
     public function create(User $user)
     {
-        return $user->hasPermissionTo('leadcontroller.store');
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $user->hasPermissionTo('leadcontroller.store');
     }
 }
