f9e4dd2681dde99fedf7e063750dacc150f191fc
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:17:48 +0000

    Update DealPolicy.php

--- app/Policies/DealPolicy.php
+++ app/Policies/DealPolicy.php
@@ -30,7 +30,8 @@ class DealPolicy extends BasePolicy
      */
     public function create(User $user)
     {
-        return $user->hasPermissionTo('dealcontroller.store');
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $user->hasPermissionTo('dealcontroller.store');
     }
 
 }
