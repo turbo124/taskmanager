2c16f57036542265c905d0d0f2d1f61b8ebcf7b5
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:43:03 +0000

    Update DealPolicy.php

--- app/Policies/DealPolicy.php
+++ app/Policies/DealPolicy.php
@@ -19,7 +19,8 @@ class DealPolicy extends BasePolicy
      */
     public function view(User $user, Deal $deal)
     {
-        //
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('dealcontroller.show') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
@@ -29,10 +30,10 @@ class DealPolicy extends BasePolicy
      * @param \App\Models\Invoice $invoice
      * @return mixed
      */
-    public function update(User $user, $entity)
+    public function update(User $user, Deal $deal)
     {
         return $user->account_user()->is_admin || $user->account_user(
-            )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('dealcontroller.update') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
