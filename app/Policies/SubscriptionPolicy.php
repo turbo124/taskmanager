6a88bdf51dd97895b172484f26412fe9bd9641e4
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:23:09 +0000

    Update SubscriptionPolicy.php

--- app/Policies/SubscriptionPolicy.php
+++ app/Policies/SubscriptionPolicy.php
@@ -30,6 +30,7 @@ class SubscriptionPolicy extends BasePolicy
      */
     public function create(User $user)
     {
-        return $user->hasPermissionTo('subscriptioncontroller.store');
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $user->hasPermissionTo('subscriptioncontroller.store');
     }
 }
