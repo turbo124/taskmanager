cba21ee05e9f4402bab5adc2cc8053a0993bcd57
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:21:26 +0000

    Update ProjectPolicy.php

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -30,6 +30,7 @@ class ProjectPolicy extends BasePolicy
      */
     public function create(User $user)
     {
-        return $user->hasPermissionTo('projectcontroller.store');
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $user->hasPermissionTo('projectcontroller.store');
     }
 }
