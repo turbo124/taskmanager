769b9e78e59f4f9e43ea9e92a99dad021dec7a1c
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:24:13 +0000

    Update UserPolicy.php

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -29,6 +29,7 @@ class UserPolicy extends BasePolicy
      */
     public function create(User $user)
     {
-        return $user->hasPermissionTo('usercontroller.store');
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $user->hasPermissionTo('usercontroller.store');
     }
 }
