2dd12ce94a10183fb8eb5608743d4d7641424b06
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:22:10 +0000

    Update QuotePolicy.php

--- app/Policies/QuotePolicy.php
+++ app/Policies/QuotePolicy.php
@@ -30,6 +30,7 @@ class QuotePolicy extends BasePolicy
      */
     public function create(User $user)
     {
-        return $user->hasPermissionTo('quotecontroller.store');
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $user->hasPermissionTo('quotecontroller.store');
     }
 }
