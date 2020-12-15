5ea9ee67bec002f60ac712a6763e5b51283f9526
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:16:49 +0000

    Update CreditPolicy.php

--- app/Policies/CreditPolicy.php
+++ app/Policies/CreditPolicy.php
@@ -30,6 +30,7 @@ class CreditPolicy extends BasePolicy
      */
     public function create(User $user)
     {
-        return $user->hasPermissionTo('creditcontroller.store');
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $user->hasPermissionTo('creditcontroller.store');
     }
 }
