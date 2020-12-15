ccb3957f5105870bdd228640280192bbd6552aa3
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:45:44 +0000

    Update ExpenseCategoryPolicy.php

--- app/Policies/ExpenseCategoryPolicy.php
+++ app/Policies/ExpenseCategoryPolicy.php
@@ -19,7 +19,8 @@ class ExpenseCategoryPolicy extends BasePolicy
      */
     public function view(User $user, ExpenseCategory $expenseCategory)
     {
-        //
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('expensecategorycontroller.show') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
@@ -29,10 +30,10 @@ class ExpenseCategoryPolicy extends BasePolicy
      * @param \App\Models\Invoice $invoice
      * @return mixed
      */
-    public function update(User $user, $entity)
+    public function update(User $user, ExpenseCategory $expenseCategory)
     {
         return $user->account_user()->is_admin || $user->account_user(
-            )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('expensecategorycontroller.update') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
