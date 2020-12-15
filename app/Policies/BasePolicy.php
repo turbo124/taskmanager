b877777d13ae185b5fcc741dae0c3669e70e179a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-15 23:25:03 +0000

    Update BasePolicy.php

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -8,20 +8,6 @@ use App\Models\User;
 
 class BasePolicy
 {
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Invoice $invoice
-     * @return mixed
-     */
-    public function update(User $user, $entity)
-    {
-        return $user->account_user()->is_admin || $user->account_user(
-            )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
-    }
-
     public function restore(User $user, $entity)
     {
         return $user->account_user()->is_admin || $user->account_user(
