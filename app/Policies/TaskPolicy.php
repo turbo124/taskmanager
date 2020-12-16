0efc862fab990e1b5c549fb9b0fdf8f2d370417a
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-16 08:04:55 +0000

    Update TaskPolicy.php

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -19,7 +19,8 @@ class TaskPolicy extends BasePolicy
      */
     public function view(User $user, Task $task)
     {
-        //
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('taskcontroller.show') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
@@ -29,10 +30,10 @@ class TaskPolicy extends BasePolicy
      * @param \App\Models\Invoice $invoice
      * @return mixed
      */
-    public function update(User $user, $entity)
+    public function update(User $user, Task $task)
     {
         return $user->account_user()->is_admin || $user->account_user(
-            )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('taskcontroller.update') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
