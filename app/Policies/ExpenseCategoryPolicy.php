5282274025c6388d20a83f92f7bb0f693a292a39
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-16 08:22:52 +0000

    Update ExpenseCategoryPolicy.php

--- app/Policies/ExpenseCategoryPolicy.php
+++ app/Policies/ExpenseCategoryPolicy.php
@@ -24,6 +24,19 @@ class ExpenseCategoryPolicy extends BasePolicy
     }
 
     /**
+     * Determine whether the user can delete the model.
+     *
+     * @param \App\Models\User $user
+     * @param \App\Models\Invoice $invoice
+     * @return mixed
+     */
+    public function delete(User $user, ExpenseCategory $expenseCategory)
+    {
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
+    }
+
+    /**
      * Determine whether the user can update the model.
      *
      * @param \App\Models\User $user
