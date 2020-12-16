43c2a54b769b050f4914d3db24a4c0f04e2dd57e
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-16 08:18:13 +0000

    Update BasePolicy.php

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -14,16 +14,5 @@ class BasePolicy
             )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param \App\Models\User $user
-     * @param \App\Models\Invoice $invoice
-     * @return mixed
-     */
-    public function delete(User $user, $entity)
-    {
-        return $user->account_user()->is_admin || $user->account_user(
-            )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
-    }
+   
 }
