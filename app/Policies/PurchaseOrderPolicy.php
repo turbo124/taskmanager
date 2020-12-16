c68d4be06d13f8b339e8ef8977615ede94f702d0
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-16 00:02:31 +0000

    Update PurchaseOrderPolicy.php

--- app/Policies/PurchaseOrderPolicy.php
+++ app/Policies/PurchaseOrderPolicy.php
@@ -19,7 +19,8 @@ class PurchaseOrderPolicy extends BasePolicy
      */
     public function view(User $user, PurchaseOrder $purchaseOrder)
     {
-        //
+        return $user->account_user()->is_admin || $user->account_user(
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('purchaseordercontroller.show') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
@@ -29,10 +30,10 @@ class PurchaseOrderPolicy extends BasePolicy
      * @param \App\Models\Invoice $invoice
      * @return mixed
      */
-    public function update(User $user, $entity)
+    public function update(User $user, PurchaseOrder $purchaseOrder)
     {
         return $user->account_user()->is_admin || $user->account_user(
-            )->is_owner || $entity->user_id === $user->id || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
+            )->is_owner || $entity->user_id === $user->id || $user->hasPermissionTo('purchaseordercontroller.update') || (!empty($entity->assigned_to) && $entity->assigned_to === $user->id);
     }
 
     /**
