814c3fd9c7860fc04aef0c6eddb6fcf0295250d3
michael-hampton <michaelhamptondesign@yahoo.com>
2020-12-20 12:35:02 +0000

    Create BaseValidationFilter.php

---
+++ app/Components/Import/BaseValidationFilter.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Components\Import;
+
+abstract class BaseValidationFilter
+{
+    use NameableTrait;
+
+    /**
+     * No need to attach the filter to any fields, since it will receive full csv array line if true
+     *
+     * @var bool
+     */
+    public $global = false;
+
+    /**
+     * @param mixed $value
+     * @return bool
+     */
+    abstract public function filter($value);
+}
