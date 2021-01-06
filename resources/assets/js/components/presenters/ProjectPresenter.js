128b81cf4080c936df008e672bbf34b126490b1d
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-06 20:46:31 +0000

    Update ProjectPresenter.js

--- resources/assets/js/components/presenters/ProjectPresenter.js
+++ resources/assets/js/components/presenters/ProjectPresenter.js
@@ -7,24 +7,18 @@ export default function ProjectPresenter (props) {
     switch (field) {
         case 'assigned_to': {
             const assigned_user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.assigned_to))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''}</td>
+            return assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''
         }
         case 'user_id': {
             const user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.user_id))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{`${user[0].first_name} ${user[0].last_name}`}</td>
+            return `${user[0].first_name} ${user[0].last_name}`
         }
         case 'is_completed':
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.name, props.edit)} key={field}
-                data-label={field}><i className="fa fa-check"/></td>
+            return <i className="fa fa-check"/>
         case 'due_date':
         case 'created_at':
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>
-                <FormatDate field={field} date={entity[field]}/></td>
+            return <FormatDate field={field} date={entity[field]}/>
         default:
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.name, props.edit)} key={field}
-                data-label={field}>{entity[field]}</td>
+            return entity[field]
     }
 }
