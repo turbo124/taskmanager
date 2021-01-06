e776e23509b2f91dc9f564cabcb353297f0da934
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-06 20:52:31 +0000

    Update RecurringInvoicePresenter.js

--- resources/assets/js/components/presenters/RecurringInvoicePresenter.js
+++ resources/assets/js/components/presenters/RecurringInvoicePresenter.js
@@ -17,24 +17,20 @@ export default function RecurringInvoicePresenter (props) {
     switch (field) {
         case 'assigned_to': {
             const assigned_user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.assigned_to))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''}</td>
+            return assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''
         }
         case 'user_id': {
             const user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.user_id))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{`${user[0].first_name} ${user[0].last_name}`}</td>
+            return `${user[0].first_name} ${user[0].last_name}`
         }
         case 'number_of_occurrances':
-            return <td>{entity.is_never_ending ? translations.never_ending : entity.number_of_occurrances}</td>
+            return entity.is_never_ending ? translations.never_ending : entity.number_of_occurrances
         case 'frequency':
-            return <td>{translations[frequencyOptions[entity.frequency]]}</td>
+            return translations[frequencyOptions[entity.frequency]]
         case 'total':
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label="Total">{
-                    <FormatMoney
+            return <FormatMoney
                         customers={props.customers} customer_id={entity.customer_id}
-                        amount={entity.total}/>}</td>
+                        amount={entity.total}/>
         case 'date':
         case 'due_date':
         case 'created_at':
@@ -42,36 +38,29 @@ export default function RecurringInvoicePresenter (props) {
         case 'last_sent_date':
         case 'date_to_send':
         case 'expiry_date': {
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}><FormatDate
-                    field={field} date={entity[field]}/></td>
+            return <FormatDate
+                    field={field} date={entity[field]}/>
         }
 
         case 'auto_billing_enabled':
-            return <td onClick={() => this.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{entity[field] === true ? translations.yes : translations.no}</td>
+            return entity[field] === true ? translations.yes : translations.no
 
         case 'status_field':
             return status
         case 'status_id':
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label="Status">{status}</td>
+            return status
 
         case 'customer_id': {
             const index = props.customers.findIndex(customer => customer.id === entity[field])
-            const customer = props.customers[index]
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number)}
-                data-label="Customer">{customer.name}</td>
+            const customer.name
         }
 
         case 'currency_id': {
             const currency = JSON.parse(localStorage.getItem('currencies')).filter(currency => currency.id === parseInt(props.entity.currency_id))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{currency.length ? currency[0].iso_code : ''}</td>
+            return currency.length ? currency[0].iso_code : ''
         }
 
         default:
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)} key={field}
-                data-label={field}>{entity[field]}</td>
+            return entity[field]
     }
 }
