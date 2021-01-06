a5e1c5593045d289cbb0ec7fc525fadf9676b41e
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-06 21:17:48 +0000

    Update CasePresenter.js

--- resources/assets/js/components/presenters/CasePresenter.js
+++ resources/assets/js/components/presenters/CasePresenter.js
@@ -21,37 +21,30 @@ export default function CasePresenter (props) {
         case 'priority_field':
             return priority
         case 'link_type':
-            return <td>{entity.link_type.toString().length ? caseLinkTypes[entity.link_type] : ''}</td>
+            return entity.link_type.toString().length ? caseLinkTypes[entity.link_type] : ''
 
         case 'status_id':
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label="Status">{status}</td>
+            return status
         case 'priority_id':
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label="Priority">{priority}</td>
+            return priority
         case 'customer_id': {
             const index = props.customers.findIndex(customer => customer.id === entity[field])
             const customer = props.customers[index]
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label="Customer">{customer.name}</td>
+            return customer.name
         }
         case 'assigned_to': {
             const assigned_user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.assigned_to))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''}</td>
+            return assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''
         }
         case 'user_id': {
             const user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.user_id))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{`${user[0].first_name} ${user[0].last_name}`}</td>
+            return `${user[0].first_name} ${user[0].last_name}`
         }
         case 'date':
         case 'due_date':
         case 'created_at':
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)} data-label="Date">
-                <FormatDate field={field} date={entity[field]}/></td>
+            return <FormatDate field={field} date={entity[field]}/>
         default:
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)} key={field}
-                data-label={field}>{entity[field]}</td>
+            return entity[field]
     }
 }
