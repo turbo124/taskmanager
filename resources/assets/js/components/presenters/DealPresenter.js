1ceeb78dde203f88991b216bb058c3ce934dc13d
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-06 20:48:14 +0000

    Update DealPresenter.js

--- resources/assets/js/components/presenters/DealPresenter.js
+++ resources/assets/js/components/presenters/DealPresenter.js
@@ -7,18 +7,15 @@ export default function DealPresenter (props) {
     switch (field) {
         case 'assigned_to': {
             const assigned_user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.assigned_to))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''}</td>
+            return assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''
         }
         case 'user_id': {
             const user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.user_id))
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
-                data-label={field}>{`${user[0].first_name} ${user[0].last_name}`}</td>
+            return `${user[0].first_name} ${user[0].last_name}`
         }
         case 'due_date':
-            return <td><FormatDate date={entity[field]}/></td>
+            return <FormatDate date={entity[field]}/>
         default:
-            return <td onClick={() => props.toggleViewedEntity(entity, entity.title, props.edit)} key={field}
-                data-label={field}>{entity[field]}</td>
+            return entity[field]
     }
 }
