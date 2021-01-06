8ed2050dacc3743872df506791f9530172ef0277
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-06 22:23:54 +0000

    Update UserPresenter.js

--- resources/assets/js/components/presenters/UserPresenter.js
+++ resources/assets/js/components/presenters/UserPresenter.js
@@ -4,6 +4,10 @@ export default function UserPresenter (props) {
     const { field, entity } = props
 
     switch (field) {
+        case 'name': {
+            return `${entity.first_name} ${entity.last_name}`
+        }
+
         default:
             return entity[field]
     }
