614f1d646c2c85b9d200ceef9a1a5ee763805d15
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-08 23:31:58 +0000

    Update _colors.js

--- resources/assets/js/components/utils/_colors.js
+++ resources/assets/js/components/utils/_colors.js
@@ -1,3 +1,36 @@
+function rgbToYIQ({r, g, b}) {
+    return ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  }
+
+  function hexToRgb(hex) {
+    if (!hex || hex === undefined || hex === '') {
+      return undefined;
+    }
+
+    const result =
+          /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : undefined;
+  }
+
+  export function contrast(colorHex, threshold = 128) {
+    if (colorHex === undefined) {
+      return '#000';
+    }
+
+    const rgb = hexToRgb(colorHex);
+
+    if (rgb === undefined) {
+      return '#000';
+    }
+
+    return rgbToYIQ(rgb) >= threshold ? '#000' : '#fff';
+  }
+
 export function convertHexStringToColor (value) {
     if (value == null) {
         return null
