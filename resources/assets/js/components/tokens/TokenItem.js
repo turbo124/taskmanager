5e6b0f2be41e805f45e158b3817b7d307ec56708
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-10 20:14:24 +0000

    Update TokenItem.js

--- resources/assets/js/components/tokens/TokenItem.js
+++ resources/assets/js/components/tokens/TokenItem.js
@@ -11,7 +11,24 @@ export default class TokenItem extends Component {
     constructor (props) {
         super(props)
 
+        this.state = {
+            width: window.innerWidth,
+        }
+
         this.deleteToken = this.deleteToken.bind(this)
+        this.handleWindowSizeChange = this.handleWindowSizeChange.bind(this)
+    }
+
+    componentWillMount () {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount () {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange () {
+        this.setState({ width: window.innerWidth });
     }
 
     deleteToken (id, archive = false) {
@@ -64,7 +81,7 @@ export default class TokenItem extends Component {
                     ? <ActionsMenu edit={editButton} delete={deleteButton} archive={archiveButton}
                         restore={restoreButton}/> : null
 
-                const is_mobile = window.innerWidth <= 768
+                const is_mobile = this.state.width <= 500
                 const list_class = !Object.prototype.hasOwnProperty.call(localStorage, 'dark_theme') || (localStorage.getItem('dark_theme') && localStorage.getItem('dark_theme') === 'true')
                     ? 'list-group-item-dark' : ''
 
