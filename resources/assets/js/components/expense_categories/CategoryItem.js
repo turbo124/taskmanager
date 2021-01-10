df1bf0a4258422bcb0ec7d03b557502fe3a3aa43
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-10 19:21:19 +0000

    Update CategoryItem.js

--- resources/assets/js/components/expense_categories/CategoryItem.js
+++ resources/assets/js/components/expense_categories/CategoryItem.js
@@ -81,6 +81,8 @@ export default class CategoryItem extends Component {
                         restore={restoreButton}/> : null
 
                 const is_mobile = this.state.width <= 500
+                const list_class = !Object.prototype.hasOwnProperty.call(localStorage, 'dark_theme') || (localStorage.getItem('dark_theme') && localStorage.getItem('dark_theme') === 'true')
+                    ? 'list-group-item-dark' : ''
 
                 if (!this.props.show_list) {
                     return <tr className={selectedRow} key={category.id}>
@@ -93,7 +95,7 @@ export default class CategoryItem extends Component {
                     </tr>
                 }
 
-                return is_mobile ? <div className="d-flex d-inline list-group-item-dark">
+                return is_mobile ? <div className={`d-flex d-inline ${list_class}`}>
                     <div className="list-action">
                         {!!this.props.onChangeBulk &&
                         <Input checked={isChecked} className={checkboxClass} value={category.id} type="checkbox"
@@ -105,12 +107,12 @@ export default class CategoryItem extends Component {
                     <ListGroupItem
                         onClick={() => this.props.toggleViewedEntity(category, category.name, editButton)}
                         key={index}
-                        className="border-top-0 list-group-item-dark list-group-item-action flex-column align-items-start">
+                        className={`border-top-0 list-group-item-action flex-column align-items-start ${list_class}`}>
                         <div className="d-flex w-100 justify-content-between">
                             <h5 className="mb-1">{category.name}</h5>
                         </div>
                     </ListGroupItem>
-                </div> : <div className="d-flex d-inline list-group-item-dark">
+                </div> : <div className={`d-flex d-inline ${list_class}`}>
                     <div className="list-action">
                         {!!this.props.onChangeBulk &&
                         <Input checked={isChecked} className={checkboxClass} value={category.id} type="checkbox"
@@ -122,7 +124,7 @@ export default class CategoryItem extends Component {
                     <ListGroupItem
                         onClick={() => this.props.toggleViewedEntity(category, category.name, editButton)}
                         key={index}
-                        className="border-top-0 list-group-item-dark list-group-item-action flex-column align-items-start">
+                        className={`border-top-0 list-group-item-action flex-column align-items-start ${list_class}`}>
                         <div className="d-flex w-100 justify-content-between">
                             <h5 className="mb-1">{category.name}</h5>
                         </div>
